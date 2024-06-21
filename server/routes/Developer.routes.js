7cb9d08dfdeea653c7139726eba75d09925ec07f
Marc Edison <suanieeee@yahoo.com>
2024-06-22 02:11:37 +0800

    feat: Add Developer routes
    This commit adds the Developer routes to the server/routes/Developer.routes.js file. The routes include endpoints for creating a developer account, creating a developer token, and getting all tokens. This change is necessary to handle developer-related operations in the server and improve the organization of the codebase.

---
+++ server/routes/Developer.routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+const DeveloperController = require('../controllers/Developer.controller')
+const DeveloperMiddleware = require('../middleware/Developer.middleware')
+
+router.post('/createdeveloperaccount',
+    DeveloperMiddleware.CreateDeveloperCheckEmptyFields,
+    DeveloperController.CreateDeveloperAccount
+)
+
+router.post('/createdevelopertoken',
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    DeveloperMiddleware.CreateDeveloperTokenCheckEmptyFields,
+    DeveloperController.CreateDeveloperToken
+)
+
+router.get('/tokens',
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    DeveloperController.GetAllTokens
+)
+
+module.exports = router
