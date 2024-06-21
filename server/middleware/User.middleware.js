3c01349c350c7748dcb9f96ab185ce7de001de31
Marc Edison <suanieeee@yahoo.com>
2024-06-22 03:00:53 +0800

    refactor: Update User middleware to include next() calls

--- server/middleware/User.middleware.js
+++ server/middleware/User.middleware.js
@@ -1,37 +1,37 @@
 const UserModel = require('../models/Account.model')
 
 const UserMidlleware = {
-    CheckUserTokenValid: async (req, res) => {
+    CheckUserTokenValid: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckUserTokenValid in user middleware error ${error}` });
         }
     },
-    CheckDeveloperTokenValid: async (req, res) => {
+    CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in user middleware error ${error}` });
         }
     },
     CreateUserCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateUserCheckEmptyFields in user middleware error ${error}` });
         }
     },
     CreateUserCheckUserIfExists: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateUserCheckUserIfExists in user middleware error ${error}` });
         }
     },
     UpdateUserCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `UpdateUserCheckEmptyFields in user middleware error ${error}` });
         }
