3828f88b875ebb060169ebbab248f40255546b3d
Marc Edison <suanieeee@yahoo.com>
2024-06-22 03:00:49 +0800

    refactor: Update Transaction middleware to include next() calls

--- server/middleware/Transaction.middleware.js
+++ server/middleware/Transaction.middleware.js
@@ -1,16 +1,16 @@
 const TransactionModel = require('../models/Transactions.model')
 
 const TransactionMidlleware = {
-    CheckDeveloperTokenValid: async (req, res) => {
+    CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in transaction middleware error ${error}` });
         }
     },
     CreateTransactionCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateTransactionCheckEmptyFields in transaction middleware error ${error}` });
         }
