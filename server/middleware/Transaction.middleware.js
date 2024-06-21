c97b5b755a05f9f1e5e873d2777eadee9736b34e
Marc Edison <suanieeee@yahoo.com>
2024-06-22 01:44:47 +0800

    feat: Add CheckDeveloperTokenValid function to Transaction middleware
    This commit adds the CheckDeveloperTokenValid function to the Transaction middleware in the server/middleware/Transaction.middleware.js file. The function is responsible for checking the validity of the developer token. This change is necessary to enhance security and ensure that only authorized developers can access certain transaction-related operations in the server.

--- server/middleware/Transaction.middleware.js
+++ server/middleware/Transaction.middleware.js
@@ -1,6 +1,13 @@
 const TransactionModel = require('../models/Transactions.model')
 
 const TransactionMidlleware = {
+    CheckDeveloperTokenValid: async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).json({ error: `CheckDeveloperTokenValid in transaction middleware error ${error}` });
+        }
+    },
     CreateTransactionCheckEmptyFields: async (req, res, next) => {
         try {
 
