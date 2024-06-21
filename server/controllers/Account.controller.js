8b651236db3c95746116f36405fae823467989ab
Marc Edison <suanieeee@yahoo.com>
2024-06-22 02:26:42 +0800

    feat: Add CRUD functionality for Account operations
    This commit adds CRUD (Create, Read, Update, Delete) functionality for Account operations in the server/controllers/Account.controller.js file. It includes functions for creating an account, fetching all accounts, searching for a specific account, updating an account, and updating the active status of an account. These changes are necessary to handle account-related operations in the server and improve the organization of the codebase.

--- server/controllers/Account.controller.js
+++ server/controllers/Account.controller.js
@@ -3,35 +3,44 @@ const AccountModel = require('../models/Account.model')
 const AccountController = {
     CreateAccount: async (req, res) => {
         try {
-
+            const values = req.body
+            console.log('Create Account Controller: ', values)
+            res.json({ success: true, message: 'Account created successfully!', values })
         } catch (error) {
             res.status(400).json({ error: `CreateAccount in account controller error ${error}` });
         }
     },
     GetAllAccount: async (req, res) => {
         try {
-
+            const data = await AccountModel.find()
+            res.json({ success: true, message: 'Fetch accounts successfully!', data })
         } catch (error) {
             res.status(400).json({ error: `GetAllAccount in account controller error ${error}` });
         }
     },
     SearchAccount: async (req, res) => {
         try {
-
+            const { accountId } = req.params
+            console.log('Search Account Controller: ', accountId)
+            res.json({ success: true, message: 'Fethced certain account successfully!', accountId })
         } catch (error) {
             res.status(400).json({ error: `SearchAccount in account controller error ${error}` });
         }
     },
     UpdateAccount: async (req, res) => {
         try {
-
+            const values = req.body
+            console.log('Update Account Controller: ', values)
+            res.json({ success: true, message: 'Account updated successfully!', values })
         } catch (error) {
             res.status(400).json({ error: `UpdateAccount in account controller error ${error}` });
         }
     },
     UpdateActiveAccount: async (req, res) => {
         try {
-
+            const values = req.body
+            console.log('Update Active Account Controller: ', values)
+            res.json({ success: true, message: 'Account active updated successfully!', values })
         } catch (error) {
             res.status(400).json({ error: `UpdateActiveAccount in account controller error ${error}` });
         }
