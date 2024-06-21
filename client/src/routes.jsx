f342e0434e5e6b12cce873998c45aff3ec6b891c
Marc Edison <suanieeee@yahoo.com>
2024-06-22 00:55:43 +0800

    feat: Add SignUp page to routes
    This commit adds the SignUp page to the routes in the client/src/routes.jsx file. The SignUp page is now accessible at the '/signup' path.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Home/Login'
+import SignUp from './pages/Home/SignUp'
 
 const Routes = createBrowserRouter([
   { path: '/', element: <Home  /> },
-  { path: '/login', element: <Login /> }
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> }
 ])
 
 export default Routes
