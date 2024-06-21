b0e7723cfc3fcd27d6a06efeeebfd1c55849a814
Marc Edison <suanieeee@yahoo.com>
2024-06-22 03:00:46 +0800

    refactor: Update Developer middleware to include next() calls

--- server/middleware/Developer.middleware.js
+++ server/middleware/Developer.middleware.js
@@ -1,23 +1,23 @@
 const DeveloperModel = require('../models/Developer.model')
 
 const DeveloperMiddleware = {
-    CheckDeveloperTokenValid: async (req, res) => {
+    CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
         }
     },
     CreateDeveloperCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateDeveloperCheckEmptyFields in developer middleware error ${error}` });
         }
     },
     CreateDeveloperTokenCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateDeveloperTokenCheckEmptyFields in developer middleware error ${error}` });
         }
