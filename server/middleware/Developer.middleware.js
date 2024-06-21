1f2473eff3eb2da972a2844c071a44bbbe91e39e
Marc Edison <suanieeee@yahoo.com>
2024-06-22 02:11:24 +0800

    feat: Add Developer middleware
    This commit adds the Developer middleware to the server/middleware/Developer.middleware.js file. The middleware includes functions for checking the validity of a developer token, checking for empty fields during developer creation, and checking for empty fields during developer token creation. This change is necessary to handle developer-related operations in the server and improve data validation and error handling.

---
+++ server/middleware/Developer.middleware.js
@@ -0,0 +1,27 @@
+const DeveloperModel = require('../models/Developer.model')
+
+const DeveloperMiddleware = {
+    CheckDeveloperTokenValid: async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
+        }
+    },
+    CreateDeveloperCheckEmptyFields: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            res.status(400).json({ error: `CreateDeveloperCheckEmptyFields in developer middleware error ${error}` });
+        }
+    },
+    CreateDeveloperTokenCheckEmptyFields: async (req, res, next) => {
+        try {
+
+        } catch (error) {
+            res.status(400).json({ error: `CreateDeveloperTokenCheckEmptyFields in developer middleware error ${error}` });
+        }
+    }
+}
+
+module.exports = DeveloperMiddleware
