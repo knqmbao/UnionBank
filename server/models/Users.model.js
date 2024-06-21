a1cebe3ae3dd932e61e8da098a5dfc02af9d9528
Marc Edison <suanieeee@yahoo.com>
2024-06-22 03:00:57 +0800

    refactor: Update mobileno field type to string in Users.model.js

--- server/models/Users.model.js
+++ server/models/Users.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
     },
     mobileno: {
-        type: Number,
+        type: String,
         required: true,
         unique: true
     },
