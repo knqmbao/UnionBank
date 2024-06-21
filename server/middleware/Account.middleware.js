37b7cd671e54c5577778a3d460dbeebe9685539b
Marc Edison <suanieeee@yahoo.com>
2024-06-22 03:00:42 +0800

    refactor: Update Account middleware to include next() calls

--- server/middleware/Account.middleware.js
+++ server/middleware/Account.middleware.js
@@ -1,30 +1,30 @@
 const AccountModel = require('../models/Account.model')
 
 const AccountMidlleware = {
-    CheckDeveloperTokenValid: async (req, res) => {
+    CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in account middleware error ${error}` });
         }
     },
     CreateAccountCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CreateCheckEmptyFields in account middleware error ${error}` });
         }
     },
     CreateAccountCheckAccountIfExists: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `CheckAccountIfExists in account middleware error ${error}` });
         }
     },
     UpdateAccountCheckEmptyFields: async (req, res, next) => {
         try {
-
+            next()
         } catch (error) {
             res.status(400).json({ error: `UpdateAccountCheckEmptyFields in account middleware error ${error}` });
         }
