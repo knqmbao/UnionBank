a52f7de32c54fcc65b728aa81eaf8e82f8991449
sdevmarc <suanieeee@yahoo.com>
2024-06-22 21:18:59 +0800

    refactor: Improve token verification in CheckDeveloperTokenValid middleware

--- server/middleware/Account.middleware.js
+++ server/middleware/Account.middleware.js
@@ -3,13 +3,30 @@ const DeveloperModel = require('../models/Developer.model')
 const bcrypt = require('bcrypt')
 
 const AccountMidlleware = {
+    CheckUserTokenValid: async (req, res, next) => {
+        try {
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if (token === null) return res.json({ authorization: `You are not authorized: null` })
+            if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
+
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+                // if (err) return res.sendStatus(403)
+                if (err) return res.json({ authorization: `You are not authorized. : ${err}` })
+                req.userId = decoded.userId
+                next()
+            })
+        } catch (error) {
+            res.status(400).json({ error: `CheckUserTokenValid in user middleware error ${error}` });
+        }
+    },
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
             const authHeader = req.headers['authorization']
             const token = authHeader && authHeader.split(' ')[1]
             if (token === null) return res.json({ authorization: `You are not authorized: null` })
             if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
-                
+
             const testToken = await DeveloperModel.findOne(token)
             if (token !== testToken) return res.json({ success: false, message: 'A token is required!' })
             next()
