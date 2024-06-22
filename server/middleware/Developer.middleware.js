f9b599ef73572ed658bf476608ac6bd1de0fefe3
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:19:55 +0800

    refactor: Add jwt token verification to CheckDeveloperTokenValid middleware
    This commit adds jwt token verification to the CheckDeveloperTokenValid middleware in Developer.middleware.js. It extracts the token from the authorization header, verifies it using the SECRET_TOKEN from the environment variables, and sets the decoded userId in the request object. This change enhances the security and functionality of the codebase.

--- server/middleware/Developer.middleware.js
+++ server/middleware/Developer.middleware.js
@@ -1,5 +1,7 @@
 const DeveloperModel = require('../models/Developer.model')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const DeveloperMiddleware = {
     // CreateDeveloperAccountCheckSccountIfExists: async (req, res, next) => {
@@ -25,7 +27,17 @@ const DeveloperMiddleware = {
     },
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-            next()
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if (token === null) return res.json({ authorization: `You are not authorized: null` })
+            if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
+
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+                // if (err) return res.sendStatus(403)
+                if (err) return res.json({ authorization: `You are not authorized. : ${err}` })
+                req.userId = decoded.userId
+                next()
+            })
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
         }
