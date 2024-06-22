fc190c13b1516c904025e088fce05c44abc16eb4
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:44:45 +0800

    refactor: Add token verification to CheckDeveloperTokenValid middleware

--- server/middleware/Transaction.middleware.js
+++ server/middleware/Transaction.middleware.js
@@ -1,9 +1,22 @@
 const TransactionModel = require('../models/Transactions.model')
+const DeveloperModel = require('../models/Developer.model')
+const bcrypt = require('bcrypt')
 
 const TransactionMidlleware = {
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-            next()
+            const { developerId } = req.body
+            const user = await DeveloperModel.findById(developerId)
+            if (user) {
+                const testToken = bcrypt.compare(developerId, user.token)
+                if (testToken) {
+                    next()
+                } else {
+                    res.json({ success: false, message: 'Token do not exist nor match' })
+                }
+            } else {
+                res.json({ success: false, message: 'A token is required!' })
+            }
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in transaction middleware error ${error}` });
         }
