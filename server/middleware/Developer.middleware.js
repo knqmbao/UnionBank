06d9de86252bd69fe5313ac81f104024aef49b57
sdevmarc <suanieeee@yahoo.com>
2024-06-22 19:15:20 +0800

    refactor: Update Developer middleware to include next() calls

--- server/middleware/Developer.middleware.js
+++ server/middleware/Developer.middleware.js
@@ -1,6 +1,21 @@
 const DeveloperModel = require('../models/Developer.model')
+const bcrypt = require('bcrypt')
 
 const DeveloperMiddleware = {
+    CreateDeveloperAccountCheckSccountIfExists: async (req, res, next) => {
+        try {
+            next()
+        } catch (error) {
+            res.status(400).json({ error: `CreateDeveloperAccountCheckSccountIfExists in developer middleware error ${error}` });
+        }
+    },
+    CheckDeveloperTokenCheckIfExist: async (req, res, next) => {
+        try {
+            next()
+        } catch (error) {
+            res.status(400).json({ error: `CheckDeveloperTokenCheckIfExist in developer middleware error ${error}` });
+        }
+    },
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
             next()
@@ -8,18 +23,23 @@ const DeveloperMiddleware = {
             res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
         }
     },
-    CreateDeveloperCheckEmptyFields: async (req, res, next) => {
+    CreateDeveloperAccountCheckEmptyFields: async (req, res, next) => {
         try {
             next()
         } catch (error) {
-            res.status(400).json({ error: `CreateDeveloperCheckEmptyFields in developer middleware error ${error}` });
+            res.status(400).json({ error: `CreateDeveloperAccountCheckEmptyFields in developer middleware error ${error}` });
         }
     },
-    CreateDeveloperTokenCheckEmptyFields: async (req, res, next) => {
+    CreateDeveloperTokenHashed: async (req, res, next) => {
         try {
+            const min = 1000000000
+            const max = 9999999999
+            const RandomIntegers = Math.floor(Math.random() * (max - min + 1)) + min
+            const hash = bcrypt.hash(RandomIntegers.toString(), 10)
+            req.hashedToken = hash
             next()
         } catch (error) {
-            res.status(400).json({ error: `CreateDeveloperTokenCheckEmptyFields in developer middleware error ${error}` });
+            res.status(400).json({ error: `CreateDeveloperTokenHashed in developer middleware error ${error}` });
         }
     }
 }
