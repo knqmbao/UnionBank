dc6b4a848e9d898713384615174513e41bb4dbbb
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:44:50 +0800

    refactor: Add token verification to CheckDeveloperTokenValid middleware

--- server/middleware/User.middleware.js
+++ server/middleware/User.middleware.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/Users.model')
+const DeveloperModel = require('../models/Developer.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
@@ -23,7 +24,18 @@ const UserMidlleware = {
     },
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-            next()
+            const { developerId } = req.body
+            const user = await DeveloperModel.findById(developerId)
+            if (user) {
+                const testToken = bcrypt.compare(developerId, user.token)
+                if (testToken) {
+                    next()
+                } else {
+                    res.json({ success: false, message: 'Token do not exist nor match' })
+                }
+            } else {
+                res.json({success: false, message: 'A token is required!'})
+            }
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in user middleware error ${error}` });
         }
