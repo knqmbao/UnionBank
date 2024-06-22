b2cb833a5403b630401935657610deb0b12807f3
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:44:40 +0800

    refactor: Add token verification to CheckDeveloperTokenValid middleware

--- server/middleware/Account.middleware.js
+++ server/middleware/Account.middleware.js
@@ -1,9 +1,22 @@
 const AccountModel = require('../models/Account.model')
+const DeveloperModel = require('../models/Developer.model')
+const bcrypt = require('bcrypt')
 
 const AccountMidlleware = {
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
-            next()
+            const { developerId } = req.body
+            const user = await DeveloperModel.findById(developerId)
+            if (user) {
+                const testToken = bcrypt.compare(developerId, user.token)
+                if (testToken) {
+                    next()
+                } else {
+                    res.json({ success: false, message: 'Token do not exist nor match' })
+                }
+            } else {
+                res.json({ success: false, message: 'A token is required!' })
+            }
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in account middleware error ${error}` });
         }
