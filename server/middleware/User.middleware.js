13fdc3f9d0f953ba023d9a61b10e0d87830af046
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:20:01 +0800

    refactor: Update User.middleware.js for user token verification and login functionality
    This commit updates the User.middleware.js file to include user token verification and login functionality. It adds a new middleware function, CheckUserTokenValid, which extracts the token from the authorization header, verifies it using the SECRET_TOKEN from the environment variables, and sets the decoded userId in the request object. Additionally, it modifies the existing middleware function for user login to include bcrypt hashing for user passwords. These changes enhance the security and functionality of the codebase.

--- server/middleware/User.middleware.js
+++ server/middleware/User.middleware.js
@@ -6,7 +6,17 @@ require('dotenv').config()
 const UserMidlleware = {
     CheckUserTokenValid: async (req, res, next) => {
         try {
-            next()
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if (token === null) return res.json({ authorization: `You are not authorized: null` })
+            if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
+
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+                // if (err) return res.sendStatus(403)
+                if (err) return res.json({ authorization: `You are not authorized. : ${err}` })
+                req.userId = decoded.userId
+                next()
+            })
         } catch (error) {
             res.status(400).json({ error: `CheckUserTokenValid in user middleware error ${error}` });
         }
@@ -45,7 +55,7 @@ const UserMidlleware = {
             if (user) {
                 const testPassword = bcrypt.compare(password, user.password)
                 if (testPassword) {
-                    const token = jwt.sign({ userId: user._id, mobileno: user.mobileno }, process.env.SECRET_TOKEN, { expiresIn: '1d' })
+                    const token = jwt.sign({ userId: user._id }, process.env.SECRET_TOKEN, { expiresIn: '1d' })
                     res.json({ success: true, message: 'Login Successful.', token })
                 } else {
                     res.json({ success: false, message: 'Username nor Password Incorrect!' })
