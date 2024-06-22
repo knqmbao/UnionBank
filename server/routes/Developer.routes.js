98a30440d413c43beddf0a9f7c22ab3fc961cb36
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:20:40 +0800

    refactor: Remove commented out code in Developer.routes.js

--- server/routes/Developer.routes.js
+++ server/routes/Developer.routes.js
@@ -4,12 +4,6 @@ const router = express.Router()
 const DeveloperController = require('../controllers/Developer.controller')
 const DeveloperMiddleware = require('../middleware/Developer.middleware')
 
-// router.post('/createdeveloperaccount',
-//     DeveloperMiddleware.CreateDeveloperAccountCheckEmptyFields,
-//     DeveloperMiddleware.CreateDeveloperAccountCheckSccountIfExists,
-//     DeveloperController.CreateDeveloperAccount
-// )
-
 router.post('/createdevelopertoken/:accountId',
     DeveloperMiddleware.CheckDeveloperTokenValid,
     DeveloperMiddleware.CreateDeveloperTokenCheckTokenIfExist,
