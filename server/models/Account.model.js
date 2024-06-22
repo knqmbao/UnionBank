cb32f56de36afc4e871edf5b3ed9eeaaaff6a6b6
Marc Edison <suanieeee@yahoo.com>
2024-06-23 01:22:21 +0800

    refactor: Add unique constraint to user field in Account.model.js
    This commit adds a unique constraint to the user field in the Account.model.js file. This change ensures that each account is associated with a unique user, enhancing the data integrity of the codebase.

--- server/models/Account.model.js
+++ server/models/Account.model.js
@@ -4,6 +4,7 @@ const accountSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+        unique: true,
         required: true,
     },
     accountType: {
