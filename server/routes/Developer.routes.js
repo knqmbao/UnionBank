c5a344a8b359149551891182f739dcc03da042bb
sdevmarc <suanieeee@yahoo.com>
2024-06-22 20:11:55 +0800

    refactor: Update Developer routes and middleware for creating developer account and token

--- server/routes/Developer.routes.js
+++ server/routes/Developer.routes.js
@@ -4,25 +4,26 @@ const router = express.Router()
 const DeveloperController = require('../controllers/Developer.controller')
 const DeveloperMiddleware = require('../middleware/Developer.middleware')
 
-router.post('/createdeveloperaccount',
-    DeveloperMiddleware.CreateDeveloperAccountCheckEmptyFields,
-    DeveloperMiddleware.CreateDeveloperAccountCheckSccountIfExists,
-    DeveloperController.CreateDeveloperAccount
-)
+// router.post('/createdeveloperaccount',
+//     DeveloperMiddleware.CreateDeveloperAccountCheckEmptyFields,
+//     DeveloperMiddleware.CreateDeveloperAccountCheckSccountIfExists,
+//     DeveloperController.CreateDeveloperAccount
+// )
 
 router.post('/createdevelopertoken/:accountId',
-    DeveloperMiddleware.CheckDeveloperTokenCheckIfExist,
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    DeveloperMiddleware.CreateDeveloperTokenCheckTokenIfExist,
     DeveloperMiddleware.CreateDeveloperTokenHashed,
     DeveloperController.CreateDeveloperToken
 )
 
 router.get('/tokens',
-    DeveloperMiddleware.CheckDeveloperTokenCheckIfExist,
+    DeveloperMiddleware.CheckDeveloperTokenValid,
     DeveloperController.GetAllTokens
 )
 
 router.post('/deletetoken/:tokenId',
-    DeveloperMiddleware.CheckDeveloperTokenCheckIfExist,
+    DeveloperMiddleware.CheckDeveloperTokenValid,
     DeveloperController.DeleteToken
 )
 
