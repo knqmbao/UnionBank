18550be09027328f40ea7b893560bdae18abcf76
sdevmarc <suanieeee@yahoo.com>
2024-06-23 20:08:17 +0800

    refactor: Update token verification logic in Developer.middleware.js

--- server/middleware/Developer.middleware.js
+++ server/middleware/Developer.middleware.js
@@ -17,13 +17,10 @@ const DeveloperMiddleware = {
             const token = authHeader && authHeader.split(' ')[1]
             if (token === null) return res.json({ authorization: `You are not authorized: null` })
             if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
+            const testToken = await DeveloperModel.findOne({ token: token })
 
-            jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
-                // if (err) return res.sendStatus(403)
-                if (err) return res.json({ authorization: `You are not authorized. : ${err}` })
-                req.userId = decoded.userId
-                next()
-            })
+            if (testToken || token === process.env.ADMIN_TOKEN) return next()
+            res.json({ success: false, message: 'A token is required, nor token is incorrect!' })
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
         }
