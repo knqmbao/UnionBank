81ff135e7f7e33e46119881d931056464d03015e
sdevmarc <suanieeee@yahoo.com>
2024-06-23 20:08:04 +0800

    refactor: Update TransactionController to use accountId instead of account object
    This commit updates the TransactionController in Transaction.controller.js to use the accountId instead of the account object when creating transactions. It retrieves the accountId from the AccountModel based on the user associated with the account. This change improves the accuracy and functionality of the codebase.

--- server/controllers/Transaction.controller.js
+++ server/controllers/Transaction.controller.js
@@ -1,20 +1,23 @@
 const TransactionModel = require('../models/Transactions.model')
 const AccountModel = require('../models/Account.model')
+const UserModel = require('../models/Users.model')
 
 const TransactionController = {
     DepositTransaction: async (req, res) => {
         try {
             const { account, amount, transactionType } = req.body
             console.log('Deposit Transaction Controller: ', { account, amount, transactionType })
-            
-            await TransactionModel.create({ account, amount, transactionType })
+
+            const { _id: accountId } = await AccountModel.findOne({ user: account })
+
+            await TransactionModel.create({ account: accountId, amount, transactionType })
 
             const depositAmount = parseFloat(amount)
-            const { balance } = await AccountModel.findById(account)
+            const { balance } = await AccountModel.findById(accountId)
             let currentBalance = balance + depositAmount
 
             const data = await AccountModel.findByIdAndUpdate(
-                account,
+                accountId,
                 {
                     balance: currentBalance
                 },
@@ -23,7 +26,7 @@ const TransactionController = {
 
             res.json({ success: true, message: 'Deposit transaction successfully!', data })
         } catch (error) {
-            res.status(400).json({ error: `CreateTransaction in transaction controller error ${error}` });
+            res.status(400).json({ error: `DepositTransaction in transaction controller error ${error}` });
         }
     },
     WithdrawTransaction: async (req, res) => {
@@ -31,14 +34,16 @@ const TransactionController = {
             const { account, amount, transactionType } = req.body
             console.log('Withdrawal Transaction Controller: ', { account, amount, transactionType })
 
-            await TransactionModel.create({ account, amount, transactionType })
+            const { _id: accountId } = await AccountModel.findOne({ user: account })
+
+            await TransactionModel.create({ account: accountId, amount, transactionType })
 
             const withdrawAmount = parseFloat(amount)
-            const { balance } = await AccountModel.findById(account)
+            const { balance } = await AccountModel.findById(accountId)
             const currentBalance = balance - withdrawAmount
 
             const data = await AccountModel.findByIdAndUpdate(
-                account,
+                accountId,
                 {
                     balance: currentBalance
                 },
@@ -47,7 +52,7 @@ const TransactionController = {
 
             res.json({ success: true, message: 'Withdrawal transaction successfully!', data })
         } catch (error) {
-            res.status(400).json({ error: `CreateTransaction in transaction controller error ${error}` });
+            res.status(400).json({ error: `WithdrawTransaction in transaction controller error ${error}` });
         }
     },
     TransferTransaction: async (req, res) => {
@@ -55,17 +60,20 @@ const TransactionController = {
             const { debitAccount, creditAccount, amount, transactionType } = req.body
             console.log('Transfer Transaction Controller: ', { debitAccount, creditAccount, amount, transactionType })
 
-            await TransactionModel.create({ account: debitAccount, amount, transactionType })
+            const { _id: debitAccountId } = await AccountModel.findOne({ user: debitAccount })
+            const { _id: creditAccountId } = await AccountModel.findOne({ user: creditAccount })
+
+            await TransactionModel.create({ account: debitAccountId, amount, transactionType })
 
             const transferAmount = parseFloat(amount)
-            const { balance: debitBalance } = await AccountModel.findById(debitAccount)
-            const { balance: creditBalance } = await AccountModel.findById(creditAccount)
+            const { balance: debitBalance } = await AccountModel.findById(debitAccountId)
+            const { balance: creditBalance } = await AccountModel.findById(creditAccountId)
             const debitFutureBalance = debitBalance - transferAmount
             const creditFutureBalance = creditBalance + transferAmount
 
 
             const debitData = await AccountModel.findByIdAndUpdate(
-                debitAccount,
+                debitAccountId,
                 {
                     balance: debitFutureBalance
                 },
@@ -73,7 +81,7 @@ const TransactionController = {
             )
 
             const creditData = await AccountModel.findByIdAndUpdate(
-                creditAccount,
+                creditAccountId,
                 {
                     balance: creditFutureBalance
                 },
@@ -82,7 +90,7 @@ const TransactionController = {
 
             res.json({ success: true, message: 'Withdrawal transaction successfully!', debitData, creditData })
         } catch (error) {
-            res.status(400).json({ error: `CreateTransaction in transaction controller error ${error}` });
+            res.status(400).json({ error: `TransferTransaction in transaction controller error ${error}` });
         }
     },
     GetAllTransaction: async (req, res) => {
@@ -97,6 +105,10 @@ const TransactionController = {
         try {
             const { accountId } = req.params
             console.log('Search Transaction Controller: ', accountId)
+
+            const { _id: account } = await AccountModel.findOne({ user: accountId })
+            const data = await TransactionModel.find({ account: account })
+            res.json({ success: true, message: 'Search transactions successfully!', data })
         } catch (error) {
             res.status(400).json({ error: `SearchTransaction in transaction controller error ${error}` });
         }
