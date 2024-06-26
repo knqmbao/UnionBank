de054acc9b74db59263ab2291bb7d71831f1b9e6
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:17:11 +0800

    refactor: Update Dashboard component layout

--- client/src/pages/OnlineBank/Dashboard.jsx
+++ client/src/pages/OnlineBank/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header_OnlineBank from '../../components/Header_OnlineBank'
+import Sidebar from '../../components/Sidebar'
 
 export default function Dashboard() {
     return (
         <>
             <div className="flex">
-                <Header_OnlineBank />
-                <div className="w-[80%] h-screen bg-black flex justify-center items-center">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex justify-center items-center">
                     <h1>Welcome to Dashboard</h1>
 
                 </div>
