fb6c491ecec06913cfcd96e796a246e1c4f005a3
sdevmarc <suanieeee@yahoo.com>
2024-06-26 09:35:55 +0800

    refactor: Update routes to include Dashboard component
    This commit updates the routes in the routes.jsx file to include the Dashboard component. The '/' path now renders the Dashboard component, while the '/unionbank' path renders the Home component. This change improves the navigation and user experience of the application.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -3,9 +3,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Home/Login'
 import SignUp from './pages/Home/SignUp'
+import Dashboard from './pages/OnlineBank/Dashboard'
 
 const Routes = createBrowserRouter([
-  { path: '/', element: <Home  /> },
+  { path: '/', element: <Dashboard /> },
+  { path: '/unionbank', element: <Home /> },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <SignUp /> }
 ])
