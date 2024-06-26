4b622c4a2c5c1a4651279965ed94cadc253eded0
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:05:45 +0800

    refactor: Update server configuration to use localhost
    This commit updates the server configuration in vite.config.js to use 'localhost' as the host. This change ensures that the development server runs locally, improving accessibility and testing convenience.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.10.14',
-    port: '80'
+    // host: '192.168.10.14',
+    // port: '80'
+    host: 'localhost',
   }
 })
