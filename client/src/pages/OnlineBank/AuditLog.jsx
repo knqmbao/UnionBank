c5b25da84e8ae2b009208a0643f06a0e1a35e513
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:17:03 +0800

    feat: Add AuditLog page
    This commit adds the AuditLog page to the client/src/pages/OnlineBank/AuditLog.jsx file. The page displays the audit log for the online banking application, providing users with a record of all actions and events that have occurred. This enhances the system's security and allows for better tracking and monitoring of user activity.

---
+++ client/src/pages/OnlineBank/AuditLog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar'
+
+export default function AuditLog() {
+  return (
+    <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex justify-center items-center">
+                    <h1>Welcome to audit log</h1>
+                </div>
+            </div>
+        </>
+  )
+}
