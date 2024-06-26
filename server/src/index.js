b42a88da9ba23746ea0b0049792195674732c33a
sdevmarc <suanieeee@yahoo.com>
2024-06-26 09:36:18 +0800

    refactor: Update server configuration to set host and port
    This commit updates the server configuration in vite.config.js to set the host to '192.168.10.14' and the port to '80'. This change ensures that the development server runs on the specified IP address and port, allowing for easier access and testing.

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const app = require('./App');
 
 const PORT = process.env.PORT || 3001;
-const HOST = '0.0.0.0'
+const HOST = '192.168.10.14'
 
 app.listen(PORT, HOST, () => {
     console.log(`Listening: http://${HOST}:${PORT}`);
