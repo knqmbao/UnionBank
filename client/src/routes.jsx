c82f8bd895bc21cc70be3de26946aabcc7e03e78
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:05:59 +0800

    refactor: Add new routes for AccountSettings, AuditLog, BackupRestore, Employees, Security, Transactions, and Transfer
    This commit updates the routes in the routes.jsx file to include new components for AccountSettings, AuditLog, BackupRestore, Employees, Security, Transactions, and Transfer. These new routes provide access to additional features and functionalities in the online banking application, enhancing the user experience and expanding the capabilities of the system.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -4,12 +4,28 @@ import Home from './pages/Home/Home'
 import Login from './pages/Home/Login'
 import SignUp from './pages/Home/SignUp'
 import Dashboard from './pages/OnlineBank/Dashboard'
+import AccountSettings from './pages/OnlineBank/AccountSettings'
+import AuditLog from './pages/OnlineBank/AuditLog'
+import BackupRestore from './pages/OnlineBank/BackupRestore'
+import Employees from './pages/OnlineBank/Employees'
+import Security from './pages/OnlineBank/Security'
+import Transactions from './pages/OnlineBank/Transactions'
+import Transfer from './pages/OnlineBank/Transfer'
+import Developer from './pages/Home/Developer'
 
 const Routes = createBrowserRouter([
   { path: '/', element: <Dashboard /> },
   { path: '/unionbank', element: <Home /> },
   { path: '/login', element: <Login /> },
-  { path: '/signup', element: <SignUp /> }
+  { path: '/signup', element: <SignUp /> },
+  { path: '/developer', element: <Developer /> },
+  { path: '/account', element: <AccountSettings /> },
+  { path: '/auditlog', element: <AuditLog /> },
+  { path: '/backuprestore', element: <BackupRestore /> },
+  { path: '/employees', element: <Employees /> },
+  { path: '/security', element: <Security /> },
+  { path: 'transactions', element: <Transactions /> },
+  { path: '/transfer', element: <Transfer /> }
 ])
 
 export default Routes
