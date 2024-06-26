c8307c2ec248cdcf1f3eb9d6af0e13de0a94f83c
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:58:48 +0800

    feat: Add tab functionality to Transactions page
    The code changes add tab functionality to the Transactions page in the client/src/pages/OnlineBank/Transactions.jsx file. This allows users to switch between different types of transactions, such as deposit, withdrawal, and transfer. This feature enhances the user experience by providing a more organized and intuitive way to navigate and manage transactions.

--- client/src/pages/OnlineBank/Transactions.jsx
+++ client/src/pages/OnlineBank/Transactions.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../../components/Sidebar'
+import Header from '../../components/Header__dashboard'
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 export default function Transactions() {
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <>
             <div className="flex">
                 <Sidebar />
-                <div className="w-[80%] h-screen flex justify-center items-center">
-                    <h1>Welcome to transactions</h1>
+                <div className="w-[80%] h-screen flex flex-col justify-start items-start p-[1rem]">
+                    <Header />
+                    <TabContext value={value}>
+                        <div className="w-full h-[4rem]">
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <Tab label="Deposit Transaction" value="1" />
+                                <Tab label="Withdrawal Transaction" value="2" />
+                                <Tab label="Transfer Transaction" value="3" />
+                            </TabList>
+                        </div>
+                        <TabPanel value="1">Deposit Transaction</TabPanel>
+                        <TabPanel value="2">Withdrawal Transaction</TabPanel>
+                        <TabPanel value="3">Transfer Transaction</TabPanel>
+                    </TabContext>
                 </div>
             </div>
         </>
