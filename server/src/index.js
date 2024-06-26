7ec9e8abd8c6ccf65910da31b47477b64b6a120e
sdevmarc <suanieeee@yahoo.com>
2024-06-26 19:17:37 +0800

    refactor: Update server HOST variable to use environment variable
    This commit updates the server's HOST variable in the index.js file to use the environment variable process.env.devHOST instead of the hardcoded IP address '192.168.10.14'. This change allows for more flexibility in configuring the host URL for the development environment.

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const app = require('./App');
 
 const PORT = process.env.PORT || 3001;
-const HOST = '192.168.10.14'
+const HOST = process.env.devHOST
 
 app.listen(PORT, HOST, () => {
     console.log(`Listening: http://${HOST}:${PORT}`);
