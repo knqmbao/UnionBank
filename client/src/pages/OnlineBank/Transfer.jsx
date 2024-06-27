3db0cff282e2e9c0b762b87568c6e0151f0099ed
sdevmarc <suanieeee@yahoo.com>
2024-06-27 13:25:36 +0800

    feat: Add data grid component for displaying developers

--- client/src/pages/OnlineBank/Transfer.jsx
+++ client/src/pages/OnlineBank/Transfer.jsx
@@ -1,15 +1,145 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Header from '../../components/Header__dashboard'
 
 export default function Transfer() {
-  return (
-    <>
+    return (
+        <>
             <div className="flex">
                 <Sidebar />
-                <div className="w-[80%] h-screen flex justify-center items-center">
-                    <h1>Welcome to transfer</h1>
+                <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto ">
+                    <Header title={`Make a Transfer`} />
+                    <form className='w-full h-[95%] flex flex-col justify-start items-center px-[15rem]'>
+                        <div className="space-y-12 pt-[5rem] pb-[20rem]">
+                            <div className="border-b border-gray-900/10 pb-12">
+                                <h2 className="text-base font-semibold leading-7 text-gray-900">Account Information</h2>
+                                <p className="mt-1 text-sm leading-6 text-gray-600">
+                                    This information will be displayed publicly so be careful what you share.
+                                </p>
+
+                                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                    <div className="sm:col-span-2">
+                                        <label htmlFor="debitaccount" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Debit Account
+                                        </label>
+                                        <div className="mt-2">
+                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">from/</span>
+                                                <input
+                                                    type="text"
+                                                    name="debitaccount"
+                                                    id="debitaccount"
+                                                    autoComplete="debitaccount"
+                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    placeholder="37473320"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-2">
+                                        <label htmlFor="creditaccount" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Credit Account
+                                        </label>
+                                        <div className="mt-2">
+                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">to/</span>
+                                                <input
+                                                    type="text"
+                                                    name="creditaccount"
+                                                    id="creditaccount"
+                                                    autoComplete="creditaccount"
+                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    placeholder="47571120"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-span-full">
+                                        <label htmlFor="remarks" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Remarks
+                                        </label>
+                                        <div className="mt-2">
+                                            <textarea
+                                                id="remarks"
+                                                name="remarks"
+                                                rows={3}
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                defaultValue={''}
+                                            />
+                                        </div>
+                                        <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your remarks.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="border-b border-gray-900/10 pb-12">
+                                <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+                                <p className="mt-1 text-sm leading-6 text-gray-600">Provide information about yourself.</p>
+
+                                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                    <div className="sm:col-span-3">
+                                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            First name
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                type="text"
+                                                name="first-name"
+                                                id="first-name"
+                                                autoComplete="given-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="sm:col-span-3">
+                                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Last name
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="sm:col-span-4">
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Email address
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="email"
+                                                name="email"
+                                                type="email"
+                                                autoComplete="email"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full flex items-center justify-end gap-x-6">
+                                <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </div>
+            </div >
         </>
-  )
+    )
 }
