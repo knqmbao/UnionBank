2a864b2115deac9ea2987641373b428aa74627e7
sdevmarc <suanieeee@yahoo.com>
2024-06-27 13:25:28 +0800

    refactor: Update BackupRestore page layout
    The code changes in the BackupRestore page update the layout to include a sidebar and a centered main content section. This enhancement improves the visual structure of the page and ensures consistency with other pages in the online banking application.

--- client/src/pages/OnlineBank/BackupRestore.jsx
+++ client/src/pages/OnlineBank/BackupRestore.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Sidebar from '../../components/Sidebar'
 
 export default function BackupRestore() {
-  return (
-    <>
-      <div className="flex">
-        <Sidebar />
-        <div className="w-[80%] h-screen flex justify-center items-center">
-          <h1>Welcome to backup and restore</h1>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex justify-center items-center">
+                    <h1>Welcome to backup and restore</h1>
+                </div>
+            </div>
+        </>
+    )
 }
