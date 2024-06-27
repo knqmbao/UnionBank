7b8f6ce7b03c3bdf6a5f6aef7ae71f88d5860248
sdevmarc <suanieeee@yahoo.com>
2024-06-27 12:02:20 +0800

    refactor: Update server HOST variable to use environment variable

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const app = require('./App');
 
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.devHOST
+const HOST = process.env.prodHOST
 
 app.listen(PORT, HOST, () => {
     console.log(`Listening: http://${HOST}:${PORT}`);
