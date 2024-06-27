586934c704c079300d7085012a157f1f1b8caa04
sdevmarc <suanieeee@yahoo.com>
2024-06-27 12:02:15 +0800

    feat: Add data grid component for displaying transactions
    Add the @mui/x-data-grid component to the Transactions page in the OnlineBank directory. This component allows for displaying and interacting with a grid of transaction data, including columns for ID, first name, last name, age, and full name. This enhancement improves the user experience by providing a more organized and visually appealing way to view and manage transactions.

--- client/src/pages/OnlineBank/Transactions.jsx
+++ client/src/pages/OnlineBank/Transactions.jsx
@@ -5,6 +5,69 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'firstName',
+        headerName: 'First name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'lastName',
+        headerName: 'Last name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 110,
+        editable: true,
+    },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 11, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 12, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 13, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 14, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 15, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 16, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 17, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 18, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 19, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 20, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 21, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 22, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 23, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 24, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 25, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 26, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 27, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 28, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 export default function Transactions() {
     const [value, setValue] = useState('1');
@@ -16,19 +79,64 @@ export default function Transactions() {
         <>
             <div className="flex">
                 <Sidebar />
-                <div className="w-[80%] h-screen flex flex-col justify-start items-start p-[1rem]">
-                    <Header />
+                <div className="w-[80%] h-screen flex flex-col justify-start items-start p-[1rem] overflow-hidden">
+                    <Header title={`Transactions`} />
                     <TabContext value={value}>
                         <div className="w-full h-[4rem]">
-                            <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                <Tab label="Deposit Transaction" value="1" />
-                                <Tab label="Withdrawal Transaction" value="2" />
-                                <Tab label="Transfer Transaction" value="3" />
+                            <TabList onChange={handleChange}>
+                                <Tab label="Deposit Transaction" value="1" style={{ fontWeight: 'bold' }} />
+                                <Tab label="Withdrawal Transaction" value="2" style={{ fontWeight: 'bold' }} />
+                                <Tab label="Transfer Transaction" value="3" style={{ fontWeight: 'bold' }} />
                             </TabList>
                         </div>
-                        <TabPanel value="1">Deposit Transaction</TabPanel>
-                        <TabPanel value="2">Withdrawal Transaction</TabPanel>
-                        <TabPanel value="3">Transfer Transaction</TabPanel>
+                        <TabPanel value="1" className='w-full h-[90%]'>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                initialState={{
+                                    pagination: {
+                                        paginationModel: {
+                                            pageSize: 5,
+                                        },
+                                    },
+                                }}
+                                pageSizeOptions={[5, 10, 25]}
+                                checkboxSelection
+                                disableRowSelectionOnClick
+                            />
+                        </TabPanel>
+                        <TabPanel value="2" className='w-full h-[90%]'>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                initialState={{
+                                    pagination: {
+                                        paginationModel: {
+                                            pageSize: 5,
+                                        },
+                                    },
+                                }}
+                                pageSizeOptions={[5, 10, 25]}
+                                checkboxSelection
+                                disableRowSelectionOnClick
+                            />
+                        </TabPanel>
+                        <TabPanel value="3" className='w-full h-[90%]'>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                initialState={{
+                                    pagination: {
+                                        paginationModel: {
+                                            pageSize: 5,
+                                        },
+                                    },
+                                }}
+                                pageSizeOptions={[5, 10, 25]}
+                                checkboxSelection
+                                disableRowSelectionOnClick
+                            />
+                        </TabPanel>
                     </TabContext>
                 </div>
             </div>
