ec93907d0124fe5e577e2f867d7d53932786dfdb
sdevmarc <suanieeee@yahoo.com>
2024-06-28 07:50:48 +0800

    feat: Add Header__Home component and update layout in Login page
    The code changes in the Login page add the Header__Home component to display the logo and improve navigation links. Additionally, the layout is updated to include a header and adjust the flex alignment. This enhancement provides a consistent header across the application and improves the user experience.

--- client/src/pages/Home/Login.jsx
+++ client/src/pages/Home/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Header__Home from '../../components/Header__Home'
+import LoginBG from '../../assets/LoginBG.png'
 
 export default function Login() {
     const navigate = useNavigate()
@@ -13,29 +15,40 @@ export default function Login() {
     }
     return (
         <>
-            <div className="w-full h-screen flex justify-center items-center">
-                <form className='w-[30rem] h-[35rem] bg-[#F48A3D] flex flex-col justify-center items-center gap-[1rem] px-[5rem]'>
-                    <h1>UnionBank</h1>
-                    <div className="w-full flex flex-col">
-                        <h1>Mobile No.</h1>
-                        <input type="text" inputMode='numeric' className='px-[1rem]' placeholder='Enter your mobile number...' />
+            <div className="w-full h-screen flex flex-col justify-start items-center">
+                <Header__Home />
+                <div className="w-full h-full flex justify-start items-center">
+                    <div className="w-[70%] h-full flex justify-start items-center px-[10rem]">
+                        <div className="w-[30rem] h-[30rem] rounded-xl overflow-hidden shadow-[0_10px_15px_-3px_rgba(0,0,0,0.1)]">
+                            <img src={LoginBG} alt="BG" className='w-full h-full object-cover' />
+                        </div>
+
                     </div>
-                    <div className="w-full flex flex-col">
-                        <h1>Password</h1>
-                        <input type="password" className='px-[1rem]' placeholder='Enter your password...' />
+                    <div className="w-[30%] h-full flex justify-center items-center">
+                        <form className='w-[25rem] h-[30rem] bg-[#F48A3D] flex flex-col justify-center items-center gap-[1rem] px-[5rem]'>
+                            <h1>UnionBank</h1>
+                            <div className="w-full flex flex-col">
+                                <h1>Mobile No.</h1>
+                                <input type="text" inputMode='numeric' className='px-[1rem]' placeholder='Enter your mobile number...' />
+                            </div>
+                            <div className="w-full flex flex-col">
+                                <h1>Password</h1>
+                                <input type="password" className='px-[1rem]' placeholder='Enter your password...' />
+                            </div>
+                            <button onClick={handleLogin}>
+                                Login
+                            </button>
+                            <div className="w-full flex flex-col justify-start items-start">
+                                <p>
+                                    Don't have an account? <span className='cursor-pointer text-[#fff]' onClick={handleSignUp}>Sign Up.</span>
+                                </p>
+                                <p className='cursor-pointer'>
+                                    Forgot password?
+                                </p>
+                            </div>
+                        </form>
                     </div>
-                    <button onClick={handleLogin}>
-                        Login
-                    </button>
-                    <div className="w-full flex flex-col justify-start items-start">
-                        <p>
-                            Don't have an account? <span className='cursor-pointer text-[#fff]' onClick={handleSignUp}>Sign Up.</span>
-                        </p>
-                        <p className='cursor-pointer'>
-                            Forgot password?
-                        </p>
-                    </div>
-                </form>
+                </div>
             </div>
         </>
     )
