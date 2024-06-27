d36e40f03f8c3cebe30615d7f80e2998f93077d5
sdevmarc <suanieeee@yahoo.com>
2024-06-28 07:47:13 +0800

    refactor: Update server HOST variable to use environment variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.10.14',
-    port: '80'
-    // host: 'localhost',
+    // host: '192.168.10.14',
+    // port: '80'
+    host: 'localhost',
   }
 })
