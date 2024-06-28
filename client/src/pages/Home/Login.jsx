d58654971c95b424e726d352bdee66816fa77055
sdevmarc <suanieeee@yahoo.com>
2024-06-28 08:47:51 +0800

    feat: Improve Login page layout and add UnionBank logo
    The code changes in the Login page enhance the layout by improving alignment and adding the UnionBank logo. This update ensures a visually appealing login page and maintains branding consistency across the application.

--- client/src/pages/Home/Login.jsx
+++ client/src/pages/Home/Login.jsx
@@ -23,24 +23,24 @@ export default function Login() {
                         <img src={LoginBG} alt="BG" className='w-full h-full object-cover' />
                     </div>
                     <div className="w-[30%] h-full flex justify-center items-center">
-                        <form className='w-full h-full flex flex-col justify-center items-center gap-[1rem] px-[5rem]'>
+                        <form className='w-full h-full flex flex-col justify-center items-center gap-[1rem] px-[4rem]'>
                             <div className="w-full h-[5rem] flex justify-start items-center">
-                                <img src={LogoUB} alt="BG" className='w-full h-full object-cover' />
+                                <img src={LogoUB} alt="BG" className='w-full h-full object-contain' />
                             </div>
                             <div className="w-full flex flex-col">
                                 <h1>Mobile No.</h1>
-                                <input type="text" inputMode='numeric' className='px-[1rem]' placeholder='Enter your mobile number...' />
+                                <input type="text" inputMode='numeric' className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your mobile number...' />
                             </div>
                             <div className="w-full flex flex-col">
                                 <h1>Password</h1>
-                                <input type="password" className='px-[1rem]' placeholder='Enter your password...' />
+                                <input type="password" className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your password...' />
                             </div>
-                            <button onClick={handleLogin}>
+                            <button onClick={handleLogin} className='w-full py-[.6rem] rounded-lg text-[#7b7b7b] hover:bg-[#111111] hover:text-white duration-300 ease bg-[#dcdcdc] shadow-[_0_10px_15px_-3px_rgba(0,0,0,0.1)]'>
                                 Login
                             </button>
                             <div className="w-full flex flex-col justify-start items-start">
                                 <p>
-                                    Don't have an account? <span className='cursor-pointer text-[#fff]' onClick={handleSignUp}>Sign Up.</span>
+                                    Don't have an account? <span className='cursor-pointer text-yellow-500' onClick={handleSignUp}>Sign Up.</span>
                                 </p>
                                 <p className='cursor-pointer'>
                                     Forgot password?
