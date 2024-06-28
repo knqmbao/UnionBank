fb5a9f2fc653dda46d8930b87ab7ebe59a3683d6
sdevmarc <suanieeee@yahoo.com>
2024-06-28 10:02:41 +0800

    feat: Improve placeholder text for debit and credit account inputs
    The code changes in the Transfer component update the placeholder text for the debit and credit account inputs. The placeholder now displays asterisks (*) to mask the account numbers partially, providing a better user experience and ensuring privacy. This enhancement improves the security and usability of the transfer feature.

--- client/src/pages/OnlineBank/Transfer.jsx
+++ client/src/pages/OnlineBank/Transfer.jsx
@@ -32,7 +32,7 @@ export default function Transfer() {
                                                     id="debitaccount"
                                                     autoComplete="debitaccount"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                    placeholder="37473320"
+                                                    placeholder="3747****"
                                                 />
                                             </div>
                                         </div>
@@ -50,7 +50,7 @@ export default function Transfer() {
                                                     id="creditaccount"
                                                     autoComplete="creditaccount"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                    placeholder="47571120"
+                                                    placeholder="4757****"
                                                 />
                                             </div>
                                         </div>
