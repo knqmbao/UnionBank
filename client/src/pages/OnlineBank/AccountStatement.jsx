7f24ad8e18b3e2337850dc360a52614e2f355528
Marc Edison <suanieeee@yahoo.com>
2024-06-30 03:31:34 +0800

    refactor: Add AccountStatement page
    Add the AccountStatement page to the OnlineBank application. This new page allows users to view their account statements, providing them with important financial information. This refactor enhances the navigation capabilities of the application and improves the overall user experience.

---
+++ client/src/pages/OnlineBank/AccountStatement.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/Sidebar'
+import Header__Dashboard from '../../components/Header__dashboard'
+
+export default function AccountStatement() {
+    const navigate = useNavigate()
+
+    const handleGoBack = () => {
+        navigate('/')
+    }
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto">
+                    <Header__Dashboard linkName={`Dashboard`} link={`/`} title={`Account Statement`} />
+                    <div className="w-full h-[95%] px-[20rem] py-[5rem]">
+                        <div className="px-4 sm:px-0">
+                            <h3 className="text-base font-semibold leading-7 text-gray-900">Account Information</h3>
+                            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details.</p>
+                        </div>
+                        <div className="border-t border-gray-100 mt-[1.5rem] pb-[20rem]">
+                            <dl className="divide-y divide-gray-100 ">
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Account status</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Activated</dd>
+                                </div>
+                                <div className="w-full flex items-center justify-end gap-x-6 pt-[2rem]">
+                                    <button
+                                        onClick={handleGoBack}
+                                        className="rounded-md bg-[#111111] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#333333] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Go back
+                                    </button>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
