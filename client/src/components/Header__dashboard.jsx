aab4620df0a113489540e47ad8916f6d8e1acc43
Marc Edison <suanieeee@yahoo.com>
2024-06-30 02:15:31 +0800

    feat: Update Header__Dashboard component to support dynamic breadcrumb links
    The code changes in the Header__Dashboard component allow for dynamic breadcrumb links by introducing new props `linkName`, `linkName1`, `link`, and `link1`. These props can be used to customize the displayed breadcrumb links. This enhancement improves the flexibility and usability of the component.

--- client/src/components/Header__dashboard.jsx
+++ client/src/components/Header__dashboard.jsx
@@ -3,16 +3,16 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Link from '@mui/material/Link';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 
-function handleClick(event) {
-    event.preventDefault();
-    console.info('You clicked a breadcrumb.');
-}
-
-export default function Header__Dashboard({ title }) {
+export default function Header__Dashboard({ linkName,linkName1, link, link1, title }) {
     const breadcrumbs = [
-        // <Link underline="hover" key="1" color="inherit" href="/" onClick={handleClick}>
-        //     MUI
-        // </Link>,
+        linkName &&
+        <Link underline="hover" key="1" fontSize={`.9rem`} href={link}>
+            {linkName}
+        </Link>,
+         linkName1 &&
+         <Link underline="hover" key="1" fontSize={`.9rem`} href={link1}>
+             {linkName1}
+         </Link>,
         <h1 key="3" className='font-[500] text-[.9rem] text-[#111111]'>
             {title}
         </h1>,
