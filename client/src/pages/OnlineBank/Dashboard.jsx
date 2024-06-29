7e35df215dccaa2a56307a815c3e23a69fbd0e29
sdevmarc <suanieeee@yahoo.com>
2024-06-29 21:02:25 +0800

    refactor: Update Dashboard page layout and add Savings Account section
    The code changes in the Dashboard page refactor the layout and add a new section for the Savings Account. This enhancement improves the visual appeal of the page and provides users with quick access to their savings account information. Additionally, the code refactor improves code cleanliness and reduces unnecessary files, leading to better maintainability.

--- client/src/pages/OnlineBank/Dashboard.jsx
+++ client/src/pages/OnlineBank/Dashboard.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
+import Header__Dashboard from '../../components/Header__dashboard'
+import SavingsIcon from '@mui/icons-material/Savings';
 
 export default function Dashboard() {
     return (
         <>
             <div className="flex">
                 <Sidebar />
-                <div className="w-[80%] h-screen flex justify-center items-center">
-                    <h1>Welcome to Dashboard</h1>
+                <div className="w-[80%] h-screen flex flex-col justify-between items-start p-[1rem] overflow-auto">
+                    <Header__Dashboard title={`Dashboard`} />
+                    <div className="w-full h-[90%] flex flex-col justify-start items-start gap-[1rem]">
+                        <div className="w-full h-[5%]">
+                            <h1 className='text-black font-[600] text-[1.2rem]'>Your Accounts</h1>
+                        </div>
+                        <div className="w-full flex justify-start items-start gap-[1rem] flex-wrap">
+                            <div className="w-[18rem] sm:w-[20rem] md:w-[22rem] lg:w-[24rem] h-[10rem] sm:h-[11re] md:h-[12rem] lg:h-[13rem] rounded-md shadow-[_0_10px_15px_-3px_rgba(0,0,0,0.15)] flex flex-col justify-evenly bg-[#111111] items-start p-[1rem]">
+                                <h1 className='text-white font-[500] text-[.9rem]'>Savings Account</h1>
+                                <div className="w-full flex justify-start items-center gap-[1rem]">
+                                    <SavingsIcon style={{ color: 'white', fontSize: '2rem' }} />
+                                    <div className="flex flex-col justify-center items-start">
+                                        <h1 className='text-white'>REGULAR SAVINGS</h1>
+                                        <h1 className='text-white'>******1238</h1>
+                                    </div>
+                                </div>
+                                <div className="w-full flex justify-between items-center">
+                                    <h1 className='text-white'>Current Balance</h1>
+                                    <h1 className='text-white'>PHP 15,993.04</h1>
+                                </div>
+                            </div>
 
+                        </div>
+                    </div>
                 </div>
             </div>
 
