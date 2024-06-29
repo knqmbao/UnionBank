58a3fc3b57717bef74a89a3968dd2f283b213d7b
Marc Edison <suanieeee@yahoo.com>
2024-06-30 02:15:39 +0800

    refactor: Update AccountSettings component to improve layout and add edit button
    The code changes in the AccountSettings component improve the layout by adjusting the border and spacing. Additionally, an "Edit Account" button is added at the bottom of the component for easier account editing. This refactor enhances the visual appeal and usability of the AccountSettings page.

--- client/src/pages/OnlineBank/AccountSettings.jsx
+++ client/src/pages/OnlineBank/AccountSettings.jsx
@@ -15,66 +15,27 @@ export default function AccountSettings() {
                             <h3 className="text-base font-semibold leading-7 text-gray-900">Account Information</h3>
                             <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details.</p>
                         </div>
-                        <div className="mt-6 border-t border-gray-100">
-                            <dl className="divide-y divide-gray-100">
+                        <div className="border-t border-gray-100 mt-[1.5rem] pb-[20rem]">
+                            <dl className="divide-y divide-gray-100 ">
                                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                     <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
                                     <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
-                                </div>
-                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                    <dt className="text-sm font-medium leading-6 text-gray-900">Application for</dt>
-                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
                                 </div>
                                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                     <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
                                     <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
                                 </div>
                                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                    <dt className="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
-                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Account status</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Activated</dd>
                                 </div>
-                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                    <dt className="text-sm font-medium leading-6 text-gray-900">About</dt>
-                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                        Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-                                        qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-                                        pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                                    </dd>
-                                </div>
-                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                    <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-                                    <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                        <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                                            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                                <div className="flex w-0 flex-1 items-center">
-                                                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                        <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                                                        <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                                                    </div>
-                                                </div>
-                                                <div className="ml-4 flex-shrink-0">
-                                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                        Download
-                                                    </a>
-                                                </div>
-                                            </li>
-                                            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                                <div className="flex w-0 flex-1 items-center">
-                                                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                        <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                                                        <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                                                    </div>
-                                                </div>
-                                                <div className="ml-4 flex-shrink-0">
-                                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                        Download
-                                                    </a>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </dd>
+                                <div className="w-full flex items-center justify-end gap-x-6 pt-[2rem]">
+                                    <button
+                                        type="submit"
+                                        className="rounded-md bg-[#111111] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#333333] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Edit Account
+                                    </button>
                                 </div>
                             </dl>
                         </div>
