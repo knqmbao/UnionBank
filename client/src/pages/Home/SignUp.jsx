fa33325876d3aa7a3b7e849e503fae96f31bd187
sdevmarc <suanieeee@yahoo.com>
2024-06-29 21:02:03 +0800

    refactor: Update SignUp page layout and add UnionBank logo
    The code changes in the SignUp page refactor the layout and add the UnionBank logo. This enhancement improves the visual appeal of the page and ensures consistency with other pages. Additionally, the SignUp page now includes functionality to navigate to the login and signup routes, enhancing the user experience.

--- client/src/pages/Home/SignUp.jsx
+++ client/src/pages/Home/SignUp.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import LogoUB from '../../assets/LogoUB.png'
+import LoginBG from '../../assets/LoginBG.png'
 
 export default function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+    const navigate = useNavigate()
+
+    const handleSignUp = () => {
+        navigate('/signup')
+    }
+
+    const handleLogin = () => {
+        navigate('/login')
+    }
+    return (
+        <>
+            <div className="w-full h-screen flex flex-col justify-start items-center">
+                <div className="w-full h-full flex justify-start items-center">
+                    <div className="w-[70%] h-full flex justify-start items-center">
+                        <img src={LoginBG} alt="BG" className='w-full h-full object-cover' />
+                    </div>
+                    <div className="w-[30%] h-full flex justify-center items-center">
+                        <form className='w-full h-full flex flex-col justify-center items-center gap-[1rem] px-[4rem]'>
+                            <div className="w-full h-[5rem] flex justify-start items-center">
+                                <img src={LogoUB} alt="BG" className='w-full h-full object-contain' />
+                            </div>
+                            <div className="w-full flex flex-col">
+                                <h1>Full Name</h1>
+                                <input type="text" className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your full name...' />
+                            </div>
+                            <div className="w-full flex flex-col">
+                                <h1>Email</h1>
+                                <input type="text" className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your email...' />
+                            </div>
+                            {/* <div className="w-full flex flex-col">
+                                <h1>Mobile No. (Temp)</h1>
+                                <input type="text" inputMode='numeric' className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your mobile number...' />
+                            </div> */}
+                            <div className="w-full flex flex-col">
+                                <h1>Password</h1>
+                                <input type="password" className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your password...' />
+                            </div>
+                            <button onClick={handleLogin} className='w-full py-[.6rem] rounded-lg text-[#7b7b7b] hover:bg-[#111111] hover:text-white duration-300 ease bg-[#dcdcdc] shadow-[_0_10px_15px_-3px_rgba(0,0,0,0.1)]'>
+                                Sign Up
+                            </button>
+                            <div className="w-full flex flex-col justify-start items-start">
+                                <p className='text-[.8rem]'>
+                                    Already have an account? <span className='cursor-pointer text-black text-decoration-line: underline' onClick={handleLogin}>Sign In.</span>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
