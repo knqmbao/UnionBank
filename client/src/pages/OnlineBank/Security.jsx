734a48c161cdfbb5adcc13a6602ea294d5c7799b
sdevmarc <suanieeee@yahoo.com>
2024-06-29 21:02:46 +0800

    refactor: Update Security page layout and add Attachments section
    The code changes in the Security page refactor the layout and add a new section for Attachments. This enhancement improves the visual appeal of the page and allows users to view and download attached files. Additionally, the code refactor improves code cleanliness and reduces unnecessary files, leading to better maintainability.

--- client/src/pages/OnlineBank/Security.jsx
+++ client/src/pages/OnlineBank/Security.jsx
@@ -1,15 +1,86 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
+import { PaperClipIcon } from '@heroicons/react/20/solid'
+import Header from '../../components/Header__dashboard'
 
 export default function Security() {
-  return (
-    <>
+    return (
+        <>
             <div className="flex">
                 <Sidebar />
-                <div className="w-[80%] h-screen flex justify-center items-center">
-                    <h1>Welcome to security</h1>
+                <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto">
+                    <Header title={`Security`} />
+                    <div className="w-full h-[95%] px-[20rem] py-[5rem]">
+                        <div className="px-4 sm:px-0">
+                            <h3 className="text-base font-semibold leading-7 text-gray-900">Security Information</h3>
+                            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Security details.</p>
+                        </div>
+                        <div className="mt-6 border-t border-gray-100">
+                            <dl className="divide-y divide-gray-100">
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Application for</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">About</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                        Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+                                        qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+                                        pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                    <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+                                    <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                        <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
+                                            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                                                <div className="flex w-0 flex-1 items-center">
+                                                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                                                        <span className="truncate font-medium">resume_back_end_developer.pdf</span>
+                                                        <span className="flex-shrink-0 text-gray-400">2.4mb</span>
+                                                    </div>
+                                                </div>
+                                                <div className="ml-4 flex-shrink-0">
+                                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                        Download
+                                                    </a>
+                                                </div>
+                                            </li>
+                                            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                                                <div className="flex w-0 flex-1 items-center">
+                                                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                                                        <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
+                                                        <span className="flex-shrink-0 text-gray-400">4.5mb</span>
+                                                    </div>
+                                                </div>
+                                                <div className="ml-4 flex-shrink-0">
+                                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                        Download
+                                                    </a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
-  )
+    )
 }
