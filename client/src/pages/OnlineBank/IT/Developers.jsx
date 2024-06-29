01bf2d981fbdc9af7c12cf7889f1c250ab4b016f
sdevmarc <suanieeee@yahoo.com>
2024-06-29 21:05:16 +0800

    feat: Add Developers page to IT section
    The code changes add a new Developers page to the IT section of the OnlineBank application. This page displays a data grid with ID, first name, last name, age, and full name columns. This enhancement improves the functionality and usability of the IT section.

---
+++ client/src/pages/OnlineBank/IT/Developers.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import Sidebar from '../../../components/Sidebar';
+import Header__Dashboard from '../../../components/Header__dashboard';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'firstName',
+        headerName: 'First name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'lastName',
+        headerName: 'Last name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 110,
+        editable: true,
+    },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 11, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 12, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 13, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 14, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 15, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 16, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 17, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 18, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 19, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 20, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 21, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 22, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 23, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 24, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 25, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 26, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 27, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 28, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function Developers() {
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex flex-col justify-between items-start p-[1rem] overflow-hidden">
+                    <Header__Dashboard title={`Developers`} />
+                    <div className="w-full h-[90%]">
+                        <DataGrid
+                            rows={rows}
+                            columns={columns}
+                            initialState={{
+                                pagination: {
+                                    paginationModel: {
+                                        pageSize: 5,
+                                    },
+                                },
+                            }}
+                            pageSizeOptions={[5, 10, 25]}
+                            checkboxSelection
+                            disableRowSelectionOnClick
+                        />
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
