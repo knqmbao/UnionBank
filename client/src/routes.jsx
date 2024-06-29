d3f753738eddc6745d6b8310670ab7f3cd070d52
sdevmarc <suanieeee@yahoo.com>
2024-06-29 21:01:40 +0800

    ```text refactor: Update routes.jsx to reorganize page components
    The code changes in the routes.jsx file reorganize the page components by moving them into separate directories based on their respective departments. This refactor improves code organization and maintainability, making it easier to locate and manage specific components.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -5,31 +5,43 @@ import Login from './pages/Home/Login'
 import SignUp from './pages/Home/SignUp'
 import Dashboard from './pages/OnlineBank/Dashboard'
 import AccountSettings from './pages/OnlineBank/AccountSettings'
-import AuditLog from './pages/OnlineBank/AuditLog'
-import BackupRestore from './pages/OnlineBank/BackupRestore'
-import Employees from './pages/OnlineBank/Employees'
 import Security from './pages/OnlineBank/Security'
-import Transactions from './pages/OnlineBank/Transactions'
 import Transfer from './pages/OnlineBank/Transfer'
-import Developer from './pages/Home/Developer'
-import Developers from './pages/OnlineBank/Developers'
-import APIKeys from './pages/OnlineBank/APIKeys'
+import APIKeys from './pages/OnlineBank/IT/APIKeys'
+import AuditLog from './pages/OnlineBank/IT/AuditLog'
+import Developers from './pages/OnlineBank/IT/Developers'
+import Employees from './pages/OnlineBank/HR/Employees'
+import Transactions from './pages/OnlineBank/Transactions'
+import Customers from './pages/OnlineBank/RB/Customers'
+import AddCustomer from './pages/OnlineBank/RB/AddCustomer'
+import OpenAccount from './pages/OnlineBank/RB/OpenAccount'
+import AddEmployees from './pages/OnlineBank/HR/AddEmployees'
+import UpdateAccount from './pages/OnlineBank/UpdateAccount'
 
 const Routes = createBrowserRouter([
   { path: '/', element: <Dashboard /> },
   { path: '/unionbank', element: <Home /> },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <SignUp /> },
-  { path: '/developer', element: <Developer /> },
   { path: '/account', element: <AccountSettings /> },
-  { path: '/auditlog', element: <AuditLog /> },
-  { path: '/backuprestore', element: <BackupRestore /> },
-  { path: '/employees', element: <Employees /> },
+  { path: '/account/updateaccount', element: <UpdateAccount /> },
   { path: '/security', element: <Security /> },
-  { path: 'transactions', element: <Transactions /> },
+  { path: '/transactions', element: <Transactions /> },
   { path: '/transfer', element: <Transfer /> },
+
+  // HR DEPARTMENT
+  { path: '/employees', element: <Employees /> },
+  { path: '/addemployee', element: <AddEmployees /> },
+
+  //RB DEPARTMENT
+  { path: '/customers', element: <Customers /> },
+  { path: '/addcustomer', element: <AddCustomer /> },
+  { path: '/openaccount', element: <OpenAccount /> },
+
+  // IT DEPARTMENT
   { path: '/developers', element: <Developers /> },
-  { path: '/apikeys', element: <APIKeys /> }
+  { path: '/apikeys', element: <APIKeys /> },
+  { path: '/auditlog', element: <AuditLog /> }
 ])
 
 export default Routes
