112f97114de32ea3413362cdd9f1aa3187f9d240
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:28:16 +0800

    feat: Refactor account middleware to improve token validation
    Refactor the account middleware to improve token validation. Remove unnecessary code that checks for the existence of a token in the DeveloperModel. Simplify the logic by only checking if the token matches the ADMIN_TOKEN environment variable. This change enhances the security and efficiency of the token validation process.

--- server/middleware/Account.middleware.js
+++ server/middleware/Account.middleware.js
@@ -26,9 +26,8 @@ const AccountMidlleware = {
             const token = authHeader && authHeader.split(' ')[1]
             if (token === null) return res.json({ authorization: `You are not authorized: null` })
             if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
-            const testToken = await DeveloperModel.findOne({ token: token })
 
-            if (testToken || token === process.env.ADMIN_TOKEN) return next()
+            if (token === process.env.ADMIN_TOKEN) return next()
             res.json({ success: false, message: 'A token is required, nor token is incorrect!' })
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in account middleware error ${error}` });
@@ -46,6 +45,27 @@ const AccountMidlleware = {
             next()
         } catch (error) {
             res.status(400).json({ error: `CreateAccountCheckAccountIfExists in account middleware error ${error}` });
+        }
+    },
+    CreateAccountCheckAccountNo: async (req, res, next) => {
+        try {
+            const accounts = await AccountModel.find()
+            const accountParseString = accounts.map(account => parseInt(account.accountno.match(/\d+/), 10)).filter(num => !isNaN(num))
+
+            if (accounts.length > 0) {
+                let largestNumber = Math.max(...accountParseString)
+                largestNumber++
+                const accno = largestNumber.toString().padStart(9, '0')
+
+                req.accno = accno
+                next()
+            } else {
+                const accno = '000000001'
+                req.accno = accno
+                next()
+            }
+        } catch (error) {
+            res.status(400).json({ error: `CreateAccountCheckAccountNo in account middleware error ${error}` });
         }
     },
     UpdateAccountCheckEmptyFields: async (req, res, next) => {
