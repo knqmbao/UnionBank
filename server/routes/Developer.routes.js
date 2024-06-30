81628d6a62dcee8bbdf65d556141a815f56637ce
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:42:08 +0800

    feat: Add deposit, withdraw, and transfer transaction routes for developers
    Add new routes for depositing, withdrawing, and transferring transactions for developers. These routes require a valid developer token and check for empty fields in the transaction data. This enhancement improves the functionality and flexibility of the developer module.

--- server/routes/Developer.routes.js
+++ server/routes/Developer.routes.js
@@ -3,12 +3,31 @@ const router = express.Router()
 
 const DeveloperController = require('../controllers/Developer.controller')
 const DeveloperMiddleware = require('../middleware/Developer.middleware')
+const TransactionMiddleware = require('../middleware/Transaction.middleware')
 
 router.post('/createdevelopertoken/:userId',
-    DeveloperMiddleware.CheckDeveloperTokenValid,
+    DeveloperMiddleware.CheckAdminTokenValid,
     DeveloperMiddleware.CreateDeveloperTokenCheckTokenIfExist,
     DeveloperMiddleware.CreateDeveloperTokenHashed,
     DeveloperController.CreateDeveloperToken
+)
+
+router.post('/developer/deposittransaction',
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    TransactionMiddleware.CreateTransactionCheckEmptyFields,
+    DeveloperController.DepositTransaction
+)
+
+router.post('/developer/withdrawtransaction',
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    TransactionMiddleware.CreateTransactionCheckEmptyFields,
+    DeveloperController.WithdrawTransaction
+)
+
+router.post('/developer/transfertransaction',
+    DeveloperMiddleware.CheckDeveloperTokenValid,
+    TransactionMiddleware.CreateTransactionCheckEmptyFields,
+    DeveloperController.TransferTransaction
 )
 
 router.get('/tokens',
