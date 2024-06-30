d6bf3d2728d90b62aec06df3446f04b32107e131
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:27:54 +0800

    feat: Add dropdown menu for department selection in AddEmployees page
    Add a dropdown menu for department selection in the AddEmployees page of the OnlineBank HR module. This enhancement provides users with a convenient way to select the department when adding new employees, improving the user experience and ensuring accurate data entry.

--- client/src/pages/OnlineBank/HR/AddEmployees.jsx
+++ client/src/pages/OnlineBank/HR/AddEmployees.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Header__Dashboard from '../../../components/Header__dashboard'
 import Sidebar from '../../../components/Sidebar'
 import { useNavigate } from 'react-router-dom'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function AddEmployees() {
     const navigate = useNavigate()
@@ -86,7 +92,56 @@ export default function AddEmployees() {
                                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             />
                                         </div>
+                                        <div className="mt-2">
+                                            <Menu as="div" className="relative inline-block text-left">
+                                                <div>
+                                                    <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                                        Options
+                                                        <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                    </MenuButton>
+                                                </div>
+
+                                                <MenuItems
+                                                    transition
+                                                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                                >
+                                                    <div className="py-1">
+                                                        <MenuItem>
+                                                            {({ focus }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(focus ? 'bg-gray-100 text-gray-900 text-center' : 'text-gray-700', 'block px-4 py-2 text-sm text-center')}
+                                                                >
+                                                                    Human Resource
+                                                                </a>
+                                                            )}
+                                                        </MenuItem>
+                                                        <MenuItem>
+                                                            {({ focus }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(focus ? 'bg-gray-100 text-gray-900 text-center' : 'text-gray-700', 'block px-4 py-2 text-sm text-center')}
+                                                                >
+                                                                    IT Department
+                                                                </a>
+                                                            )}
+                                                        </MenuItem>
+                                                        <MenuItem>
+                                                            {({ focus }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(focus ? 'bg-gray-100 text-gray-900 text-center' : 'text-gray-700', 'block px-4 py-2 text-sm text-center')}
+                                                                >
+                                                                    Retail Banking
+                                                                </a>
+                                                            )}
+                                                        </MenuItem>
+                                                    </div>
+                                                </MenuItems>
+                                            </Menu>
+                                        </div>
                                     </div>
+
                                 </div>
                             </div>
                             <div className="w-full flex items-center justify-end gap-x-6">
