7cd91d3f0a3d8cc15a738d06fc39a962bab6f5c7
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:28:35 +0800

    refactor: Simplify token validation in Transaction middleware
    Simplify the token validation logic in the Transaction middleware by removing unnecessary code that checks for the existence of a token in the DeveloperModel. The validation now only checks if the token matches the ADMIN_TOKEN environment variable. This refactor improves the security and efficiency of the token validation process.

--- server/middleware/Transaction.middleware.js
+++ server/middleware/Transaction.middleware.js
@@ -28,9 +28,8 @@ const TransactionMidlleware = {
             const token = authHeader && authHeader.split(' ')[1]
             if (token === null) return res.json({ authorization: `You are not authorized: null` })
             if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
-            const testToken = await DeveloperModel.findOne({ token: token })
 
-            if (testToken || token === process.env.ADMIN_TOKEN) return next()
+            if (token === process.env.ADMIN_TOKEN) return next()
             res.json({ success: false, message: 'A token is required, nor token is incorrect!' })
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in transaction middleware error ${error}` });
