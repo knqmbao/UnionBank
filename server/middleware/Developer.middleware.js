5bea29e2f21d675f4495f775eeb7bbaf3c5843d0
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:42:13 +0800

    feat: Add CheckAdminTokenValid middleware for token validation
    Add the `CheckAdminTokenValid` middleware to the `Developer.middleware.js` file. This middleware is responsible for validating the admin token by checking if it matches the `ADMIN_TOKEN` environment variable. This enhancement improves the security and efficiency of the token validation process.

--- server/middleware/Developer.middleware.js
+++ server/middleware/Developer.middleware.js
@@ -11,6 +11,20 @@ const DeveloperMiddleware = {
             res.status(400).json({ error: `CheckDeveloperTokenCheckIfExist in developer middleware error ${error}` });
         }
     },
+    CheckAdminTokenValid: async (req, res, next) => {
+        try {
+            const authHeader = req.headers['authorization']
+            const token = authHeader && authHeader.split(' ')[1]
+            if (token === null) return res.json({ authorization: `You are not authorized: null` })
+            if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
+            const testToken = await DeveloperModel.findOne({ token: token })
+
+            if (token === process.env.ADMIN_TOKEN) return next()
+            res.json({ success: false, message: 'A token is required, nor token is incorrect!' })
+        } catch (error) {
+            res.status(400).json({ error: `CheckDeveloperTokenValid in developer middleware error ${error}` });
+        }
+    },
     CheckDeveloperTokenValid: async (req, res, next) => {
         try {
             const authHeader = req.headers['authorization']
