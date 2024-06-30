1969640685580780bc58206a85b1c92d578d7a4f
sdevmarc <suanieeee@yahoo.com>
2024-06-30 19:28:41 +0800

    feat: Refactor User middleware to use email instead of mobileno
    Refactor the User middleware to use the email field instead of the mobileno field for user authentication. This change improves consistency and aligns with the standard practice of using email as the primary identifier for user accounts.

--- server/middleware/User.middleware.js
+++ server/middleware/User.middleware.js
@@ -30,9 +30,8 @@ const UserMidlleware = {
             const token = authHeader && authHeader.split(' ')[1]
             if (token === null) return res.json({ authorization: `You are not authorized: null` })
             if (token === undefined) return res.json({ authorization: `You are not authorized: undefined` })
-            const testToken = await DeveloperModel.findOne({ token: token })
 
-            if (testToken || token === process.env.ADMIN_TOKEN) return next()
+            if (token === process.env.ADMIN_TOKEN) return next()
             res.json({ success: false, message: 'A token is required, nor token is incorrect!' })
         } catch (error) {
             res.status(400).json({ error: `CheckDeveloperTokenValid in user middleware error ${error}` });
@@ -40,18 +39,22 @@ const UserMidlleware = {
     },
     LoginUserCheckEmptyFields: async (req, res, next) => {
         try {
-            const { mobileno, password } = req.body
-            if (!mobileno || !password) return res.json({ success: false, message: 'Required fields should not be empty.' })
+            const { email, password } = req.body
+            if (!email || !password) return res.json({ success: false, message: 'Required fields should not be empty.' })
             next()
         } catch (error) {
             res.status(400).json({ error: `LoginUserCheckEmptyFields in user middleware error ${error}` });
         }
     },
-    LoginUserCheckMobileNo: async (req, res, next) => {
+    LoginUserCheckEmail: async (req, res, next) => {
         try {
-            const { mobileno } = req.body
-            const testMobileNo = await UserModel.findOne({ mobileno: mobileno })
-            if (mobileno !== testMobileNo.mobileno) return res.json({ success: false, message: 'User not found.', testMobileNo })
+            const { email } = req.body
+            const testEmail = await UserModel.findOne(
+                {
+                    email: { $regex: new RegExp(email, 'i') }
+                }
+            )
+            if (email !== testEmail.email) return res.json({ success: false, message: 'User not found.', testEmail })
             next()
         } catch (error) {
             res.status(400).json({ error: `LoginUserCheckUsername in user middleware error ${error}` });
@@ -59,52 +62,23 @@ const UserMidlleware = {
     },
     LoginUserCheckPassword: async (req, res) => {
         try {
-            const transporter = nodemailer.createTransport({
-                service: 'gmail', // You can use other services like Yahoo, Outlook, etc.
-                auth: {
-                    user: 'yourparengmarc@gmail.com', // Your email address
-                    pass: 'Medsako12345' // Your email password or an app-specific password
+            const { email, password } = req.body
+            const user = await UserModel.findOne(
+                {
+                    email: { $regex: new RegExp(email, 'i') }
                 }
-            });
-
-            // Verify the connection configuration
-            transporter.verify((error, success) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Server is ready to take our messages');
-                }
-            });
-
-            const mailOptions = {
-                from: 'yourparengmarc@gmail.com',
-                to: 'edisonsuarez360@gmail.com',
-                subject: 'Testing nodemailer',
-                text: 'Tite',
-                html: '<p>HTML version of the content</p>'
-              };
-              
-              transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                  return console.log(error);
-                }
-                console.log('Message sent: %s', info.messageId);
-              });
-              
-
-            const { mobileno, password } = req.body
-            const user = await UserModel.findOne({ mobileno: mobileno })
+            )
 
             if (user) {
                 const testPassword = bcrypt.compare(password, user.password)
                 if (testPassword) {
                     const token = jwt.sign({ userId: user._id }, process.env.SECRET_TOKEN, { expiresIn: '1d' })
-                    res.json({ success: true, message: 'Login Successful.', token, name: user.name, accountId: user.mobileno, userId: user._id })
+                    res.json({ success: true, message: 'Login Successful.', token, name: user.name, userId: user._id })
                 } else {
-                    res.json({ success: false, message: 'Username nor Password Incorrect!' })
+                    res.json({ success: false, message: 'Email or Password is Incorrect!' })
                 }
             } else {
-                res.json({ success: false, message: 'Username nor Password Incorrect!' })
+                res.json({ success: false, message: 'Username or Password is Incorrect!' })
             }
         } catch (error) {
             res.status(400).json({ error: `LoginUserCheckPassword in user middleware error ${error}` });
@@ -119,6 +93,19 @@ const UserMidlleware = {
     },
     CreateUserCheckUserIfExists: async (req, res, next) => {
         try {
+            const { email, mobileno } = req.body
+            const testEmail = await UserModel.find(
+                {
+                    email: { $regex: new RegExp(email, 'i') }
+                }
+            )
+            const testMobileNo = await UserModel.find(
+                {
+                    mobileno: { $regex: new RegExp(mobileno, 'i') }
+                }
+            )
+            if (testEmail.length > 0) return res.json({ success: false, message: 'Email already exists!' })
+            if (testMobileNo.length > 0) return res.json({ success: false, message: 'Mobile number already exists!' })
             next()
         } catch (error) {
             res.status(400).json({ error: `CreateUserCheckUserIfExists in user middleware error ${error}` });
