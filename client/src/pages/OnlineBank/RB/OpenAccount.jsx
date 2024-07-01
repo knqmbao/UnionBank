28d2c1dd5ca3322ee544d0cd7786b24f4da86512
sdevmarc <suanieeee@yahoo.com>
2024-07-01 15:55:27 +0800

    feat: Refactor Deposit form layout for improved visual spacing and alignment

--- client/src/pages/OnlineBank/RB/OpenAccount.jsx
+++ client/src/pages/OnlineBank/RB/OpenAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../../components/Sidebar'
 import Header__Dashboard from '../../../components/Header__dashboard'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
@@ -10,6 +10,13 @@ function classNames(...classes) {
 }
 
 export default function OpenAccount() {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        mobileno: '',
+        accountType: '',
+        amount: ''
+    })
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -19,27 +26,59 @@ export default function OpenAccount() {
     const fetchCredentials = () => {
         try {
             const credentials = localStorage.getItem('credentials')
+            const customerdetails = sessionStorage.getItem('customerdetails')
+
             if (!credentials) return navigate('/unionbank')
+            const { firstname, lastname, email, mobileno } = JSON.parse(customerdetails)
+            const fullname = firstname + " " + lastname
+
+            setValues((prev) => ({
+                ...prev,
+                name: fullname,
+                email: email,
+                mobileno: mobileno
+            }))
+
+
         } catch (error) {
             console.error(error)
         }
     }
-    const handleCancel = () => {
+    const handleBack = () => {
         navigate('/customers/addcustomer')
     }
+
+    const handleOnChangeInitDepo = (e) => {
+        const { value } = e.target
+        setValues((prev) => ({
+            ...prev,
+            amount: value
+        }))
+        console.log(values)
+    }
+
+
+    const handleAccountTypeSelect = (type) => {
+        setValues((prev) => ({
+            ...prev,
+            accountType: type
+        }));
+        console.log(type)
+    };
+
     return (
         <>
             <div className="flex">
                 <Sidebar />
                 <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto ">
                     <Header__Dashboard linkName={`Customers`} linkName1={`Add Customer`} link={`/customers`} link1={`/customers/addcustomer`} title={`Open Account`} />
-                    <form className='w-full h-[95%] flex flex-col justify-start items-center px-[20rem]'>
+                    <form className='w-full h-[95%] flex flex-col justify-start items-center px-[5rem]'>
                         <div className="space-y-12 pt-[5rem] pb-[20rem]">
                             <div className="border-b border-gray-900/10 pb-12">
                                 <h2 className="text-base font-semibold leading-7 text-gray-900">Account Type</h2>
                                 <p className="mt-1 text-sm leading-6 text-gray-600">Select account type.</p>
 
-                                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-10">
                                     <div className="sm:col-span-3">
                                         <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
                                             Account type *
@@ -48,27 +87,24 @@ export default function OpenAccount() {
                                             <Menu as="div" className="relative inline-block text-left">
                                                 <div>
                                                     <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                                        Options
+                                                        {
+                                                            values?.accountType ? values?.accountType : 'Options'
+                                                        }
                                                         <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
                                                     </MenuButton>
                                                 </div>
 
-                                                <MenuItems
-                                                    transition
-                                                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                                >
-                                                    <div className="py-1">
-                                                        <MenuItem>
-                                                            {({ focus }) => (
-                                                                <a
-                                                                    href="#"
-                                                                    className={classNames(focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}
-                                                                >
-                                                                    Savings
-                                                                </a>
-                                                            )}
-                                                        </MenuItem>
-                                                    </div>
+                                                <MenuItems transition className="absolute cursor-pointer  right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
+                                                    <MenuItem onClick={() => handleAccountTypeSelect('None')} className='hover:bg-gray-100 py-[.7rem]'>
+                                                        <h1 className={`text-gray-700 block px-4 py-2 text-sm text-center`} >
+                                                            None
+                                                        </h1>
+                                                    </MenuItem>
+                                                    <MenuItem onClick={() => handleAccountTypeSelect('Savings')} className='hover:bg-gray-100 py-[.7rem]'>
+                                                        <h1 className={`text-gray-700 block px-4 py-2 text-sm text-center`} >
+                                                            Savings
+                                                        </h1>
+                                                    </MenuItem>
                                                 </MenuItems>
                                             </Menu>
                                         </div>
@@ -77,12 +113,13 @@ export default function OpenAccount() {
                                     <div className="sm:col-span-3">
                                         <div className="sm:col-span-2">
                                             <label htmlFor="initialdeposit" className="block text-sm font-medium leading-6 text-gray-900">
-                                                Initial Deposit
+                                                Initial Deposit (Optional)
                                             </label>
                                             <div className="mt-2">
                                                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                     <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">PHP/</span>
                                                     <input
+                                                        onChange={handleOnChangeInitDepo}
                                                         type="text"
                                                         name="initialdeposit"
                                                         id="initialdeposit"
@@ -97,9 +134,9 @@ export default function OpenAccount() {
                             </div>
                             <div className="w-full flex items-center justify-end gap-x-6">
                                 <button
-                                    onClick={handleCancel}
+                                    onClick={handleBack}
                                     className="text-sm font-semibold leading-6 text-gray-900">
-                                    Cancel
+                                    Back
                                 </button>
                                 <button
                                     type="submit"
@@ -110,7 +147,7 @@ export default function OpenAccount() {
                             </div>
                         </div>
                     </form>
-                </div>
+                </div >
             </div >
         </>
     )
