e27baaeb8b4858cec9a1315554d8599e408eedbc
sdevmarc <suanieeee@yahoo.com>
2024-07-01 09:40:53 +0800

    Refactor CreateUserCheckUserIfExists middleware

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import GridViewIcon from '@mui/icons-material/GridView';
 import { NavLink, useNavigate } from 'react-router-dom';
@@ -16,6 +16,19 @@ import AddCardOutlinedIcon from '@mui/icons-material/AddCardOutlined';
 import HourglassEmptyOutlinedIcon from '@mui/icons-material/HourglassEmptyOutlined';
 
 export default function Sidebar() {
+    const [userRole, setUserRole] = useState('')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetchCredentials()
+    }, [])
+
+    const fetchCredentials = () => {
+        const credentials = localStorage.getItem('credentials')
+        if(!credentials) return navigate('/unionbank')
+        const { role } = JSON.parse(credentials)
+        setUserRole(role)
+    }
 
     const handleLogout = () => {
         localStorage.clear()
@@ -37,48 +50,69 @@ export default function Sidebar() {
                                     Dashboard
                                 </h1>
                             </NavLink>
-                            <NavLink to='/transactions' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <ReceiptLongOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Transactions
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/transfer' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <MoveDownOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Make a Transfer
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/developers' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <CodeOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Developers
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/employees' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <SupervisorAccountOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Employees
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/customers' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <AccessibilityOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Customers
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/deposit' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <AddCardOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Deposit
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/withdrawal' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <HourglassEmptyOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Withdrawal
-                                </h1>
-                            </NavLink>
+                            {
+                                (userRole === 'user' || userRole === 'developer' || userRole === 'admin') && (
+                                    <>
+                                        <NavLink to='/transactions' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <ReceiptLongOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Transactions
+                                            </h1>
+                                        </NavLink>
+                                        <NavLink to='/transfer' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <MoveDownOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Make a Transfer
+                                            </h1>
+                                        </NavLink>
+                                    </>
+                                )
+                            }
+
+                            {
+                                (userRole === 'it' || userRole === 'admin') && (
+                                    <NavLink to='/developers' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                        <CodeOutlinedIcon />
+                                        <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                            Developers
+                                        </h1>
+                                    </NavLink>
+                                )
+                            }
+                            {
+                                (userRole === 'hr' || userRole === 'admin') && (
+                                    <NavLink to='/employees' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                        <SupervisorAccountOutlinedIcon />
+                                        <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                            Employees
+                                        </h1>
+                                    </NavLink>
+                                )
+                            }
+                            {
+                                (userRole === 'rb' || userRole === 'admin') && (
+                                    <>
+                                        <NavLink to='/customers' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <AccessibilityOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Customers
+                                            </h1>
+                                        </NavLink>
+                                        <NavLink to='/deposit' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <AddCardOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Deposit
+                                            </h1>
+                                        </NavLink>
+                                        <NavLink to='/withdrawal' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <HourglassEmptyOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Withdrawal
+                                            </h1>
+                                        </NavLink>
+                                    </>
+                                )
+                            }
                         </div>
                         <h1 className='hidden sm:hidden md:hidden lg:block text-[#9CA3AF] text-[.3rem] sm:text-[.5rem] md:text-[.7rem] lg:text-[.9rem]'>Settings</h1>
                         <div className="hnavs w-full py-[1rem] flex flex-col justify-start items-start gap-[.2rem]">
@@ -88,24 +122,34 @@ export default function Sidebar() {
                                     Account
                                 </h1>
                             </NavLink>
-                            <NavLink to='/auditlog' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <BookOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Audit Log
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/security' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <GppGoodOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    Security
-                                </h1>
-                            </NavLink>
-                            <NavLink to='/apikeys' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                <HttpOutlinedIcon />
-                                <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
-                                    API Keys
-                                </h1>
-                            </NavLink>
+                            {
+                                (userRole === 'it' || userRole === 'admin') && (
+                                    <>
+                                        <NavLink to='/auditlog' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <BookOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Audit Log
+                                            </h1>
+                                        </NavLink>
+                                        <NavLink to='/security' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                            <GppGoodOutlinedIcon />
+                                            <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                                Security
+                                            </h1>
+                                        </NavLink>
+                                    </>
+                                )
+                            }
+                            {
+                                (userRole === 'developer' || userRole === 'admin') && (
+                                    <NavLink to='/apikeys' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
+                                        <HttpOutlinedIcon />
+                                        <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
+                                            API Keys
+                                        </h1>
+                                    </NavLink>
+                                )
+                            }
                         </div>
                     </div>
                     <div onClick={handleLogout} className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1] cursor-pointer hover:bg-[#d4d4d4] duration-300 ease">
