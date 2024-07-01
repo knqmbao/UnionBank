6909f5f4f52ba778de88cab24072b9dbb7a9f2e8
sdevmarc <suanieeee@yahoo.com>
2024-07-01 09:41:12 +0800

    feat: Update Home page layout and content
    Refactor the Home component in the Home.jsx file to update the layout and content. Remove the commented out code and replace it with a new layout that displays department names, corresponding email addresses, and passwords. This change improves the user experience by providing clear information about different departments and their login credentials.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,15 +7,38 @@ export default function Home() {
     return (
         <div className='w-full h-screen flex justify-start items-center flex-col'>
             <Header__Home />
-            <div className="w-full h-full p-[5rem]">
+            {/* <div className="w-full h-full p-[5rem]">
                 <img src={test} alt="asd" className='w-full h-full object-cover' />
+            </div> */}
+            <div className="w-full h-full flex justify-center items-center gap-[7rem]">
+            <div className="flex flex-col items-center gap-[1rem]">
+                    <h1 className='text-[2rem] font-bold'>Department</h1>
+                    <h1>ADMIN</h1>
+                    <h1>HUMAN RESOURCE</h1>
+                    <h1>IT DEPARTMENT</h1>
+                    <h1>RETAIL BANKING</h1>
+                    <h1>DEVELOPER</h1>
+                    <h1>USER</h1>
+                </div>
+                <div className="flex flex-col items-center gap-[1rem]">
+                    <h1 className='text-[2rem] font-bold'>Email</h1>
+                    <h1>admin@gmail.com</h1>
+                    <h1>hr@gmail.com</h1>
+                    <h1>it@gmail.com</h1>
+                    <h1>rb@gmail.com</h1>
+                    <h1>developer@gmail.com</h1>
+                    <h1>user@gmail.com</h1>
+                </div>
+                <div className="flex flex-col items-center gap-[1rem]">
+                    <h1 className='text-[2rem] font-bold'>Password</h1>
+                    <h1>admin</h1>
+                    <h1>hr</h1>
+                    <h1>it</h1>
+                    <h1>rb</h1>
+                    <h1>developer</h1>
+                    <h1>user</h1>
+                </div>
             </div>
-            {/* <h1 className='font-[600] text-[2rem]'>
-                Welcome to UnionBank
-            </h1>
-            <Link to='/login'>
-                Login
-            </Link> */}
         </div>
     )
 }
