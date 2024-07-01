680658ee7d4e9a5b8980230d908dfad9cdeb42d2
sdevmarc <suanieeee@yahoo.com>
2024-07-01 22:56:05 +0800

    feat: Add form submission functionality to Deposit component
    Refactor the Deposit component in Deposit.jsx to add form submission functionality. Implement the handleDeposit function to send a POST request to the server with the account and amount values. This enhancement improves the functionality of the application by allowing users to deposit funds into their accounts.

--- client/src/pages/OnlineBank/RB/Deposit.jsx
+++ client/src/pages/OnlineBank/RB/Deposit.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../../components/Sidebar'
 import Header__Dashboard from '../../../components/Header__dashboard'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+const { VITE_HOST, VITE_ADMIN_TOKEN } = import.meta.env
 
 export default function Deposit() {
+    const [values, setValues] = useState({
+        account: '',
+        amount: '',
+    })
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -18,13 +24,50 @@ export default function Deposit() {
             console.error(error)
         }
     }
+
+    const handleDeposit = async (e) => {
+        try {
+            e.preventDefault()
+            const { account, amount } = values
+            const res = await axios.post(`${VITE_HOST}/api/deposittransaction`, { account, amount }, {
+                headers: {
+                    Authorization: `Bearer ${VITE_ADMIN_TOKEN}`
+                }
+            })
+            if (res?.data?.success) return alert(res?.data?.message)
+            alert(res?.data?.message)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            handleCleanUp()
+        }
+    }
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target
+        setValues((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleCleanUp = () => {
+        setValues((prev) => ({
+            ...prev,
+            account: '',
+            amount: ''
+        }))
+    }
+
     return (
         <>
             <div className="flex">
                 <Sidebar />
                 <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto ">
                     <Header__Dashboard title={`Deposit`} />
-                    <form className='w-full h-[95%] flex flex-col justify-start items-center px-[5rem]'>
+                    <form
+                        onSubmit={handleDeposit}
+                        className='w-full h-[95%] flex flex-col justify-start items-center px-[5rem]'>
                         <div className="space-y-12 pt-[5rem] pb-[20rem]">
                             <div className="border-b border-gray-900/10 pb-12">
                                 <h2 className="text-base font-semibold leading-7 text-gray-900">Deposit Details</h2>
@@ -41,12 +84,15 @@ export default function Deposit() {
                                             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">to/</span>
                                                 <input
+                                                    value={values?.account}
+                                                    onChange={handleOnChange}
                                                     type="text"
-                                                    name="creditaccount"
-                                                    id="creditaccount"
-                                                    autoComplete="creditaccount"
+                                                    name="account"
+                                                    id="account"
+                                                    autoComplete="account"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                                     placeholder="4757****"
+                                                    required
                                                 />
                                             </div>
                                         </div>
@@ -59,12 +105,15 @@ export default function Deposit() {
                                             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">PHP/</span>
                                                 <input
+                                                    value={values?.amount}
+                                                    onChange={handleOnChange}
                                                     type="text"
-                                                    name="deposit"
-                                                    id="deposit"
-                                                    autoComplete="deposit"
+                                                    name="amount"
+                                                    id="amount"
+                                                    autoComplete="amount"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                                     placeholder="100.00"
+                                                    required
                                                 />
                                             </div>
                                         </div>
