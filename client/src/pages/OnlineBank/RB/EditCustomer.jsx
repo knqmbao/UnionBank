e1300b8ac5519090dd91688c1a0cf8b4cff38a4c
sdevmarc <suanieeee@yahoo.com>
2024-07-01 15:55:13 +0800

    Refactor Deposit form layout for improved visual spacing and alignment

---
+++ client/src/pages/OnlineBank/RB/EditCustomer.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Sidebar from '../../../components/Sidebar'
+import Header__Dashboard from '../../../components/Header__dashboard'
+
+export default function EditCustomer() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetchCredentials()
+    }, [])
+
+    const fetchCredentials = () => {
+        try {
+            const credentials = localStorage.getItem('credentials')
+            if (!credentials) return navigate('/unionbank')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleUpdate = (e) => {
+        e.preventDefault()
+        alert('Test Update')
+    }
+
+    const handleCancel = () => {
+        navigate('/customers')
+    }
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex flex-col justify-start items-center p-[1rem] overflow-auto ">
+                    <Header__Dashboard linkName={`Customers`} link={`/customers`} title={`Edit Customer`} />
+                    <form
+                        onSubmit={handleUpdate}
+                        className='w-full h-[95%] flex flex-col justify-start items-center px-[10rem]'>
+                        <div className="space-y-12 pt-[5rem] pb-[20rem]">
+                            <div className="border-b border-gray-900/10 pb-12">
+                                <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+                                <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+
+                                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                    <div className="sm:col-span-3">
+                                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            First name
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                type="text"
+                                                name="first-name"
+                                                id="first-name"
+                                                autoComplete="given-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="sm:col-span-3">
+                                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Last name
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="sm:col-span-4">
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Email address
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="email"
+                                                name="email"
+                                                type="email"
+                                                autoComplete="email"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-5">
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Mobile No.
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="mobileno"
+                                                name="mobileno"
+                                                type="text"
+                                                inputMode='numeric'
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-5">
+                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Password
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                type="password"
+                                                name="password"
+                                                id="password"
+                                                autoComplete="password"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-5 flex justify-between items-center">
+                                        <label htmlFor="developer" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Developer
+                                        </label>
+                                        <Toggle isCheck />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full flex items-center justify-end gap-x-6">
+                                <button
+                                    onClick={handleCancel}
+                                    className="text-sm font-semibold leading-6 text-gray-900">
+                                    Cancel
+                                </button>
+                                <button
+                                    type='submit'
+                                    className="rounded-md bg-[#111111] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#333333] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div >
+        </>
+    )
+}
