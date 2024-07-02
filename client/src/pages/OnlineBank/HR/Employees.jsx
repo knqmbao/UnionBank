402dc0fab501f68aa2a7ec8b11bde2155ba86cfc
sdevmarc <suanieeee@yahoo.com>
2024-07-02 21:45:14 +0800

    refactor: Update Employees component to include isActive toggle and action buttons

--- client/src/pages/OnlineBank/HR/Employees.jsx
+++ client/src/pages/OnlineBank/HR/Employees.jsx
@@ -6,82 +6,6 @@ import DataGrids from '../../../components/DataGrids';
 import { Link, useNavigate } from 'react-router-dom';
 import Toggle from '../../../components/Toggle';
 
-const renderIsActiveToggle = (params) => {
-    return (
-        <div className="w-full h-full flex justify-center items-center">
-            <Toggle isCheck />
-        </div>
-
-    );
-};
-
-const renderActionButtons = (params) => {
-    return (
-        <div className="w-full h-full flex justify-center items-center">
-            <Button variant="text">
-                <h1>Edit</h1>
-            </Button>
-        </div>
-    );
-};
-
-const columns = [
-    {
-        field: 'id',
-        headerName: 'No.',
-        width: 90,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'userid',
-        headerName: 'ID',
-        width: 200,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'name',
-        headerName: 'Full Name',
-        width: 250,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'email',
-        headerName: 'Email',
-        width: 250,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'isactive',
-        headerName: 'Active',
-        width: 200,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: renderIsActiveToggle
-    },
-    {
-        field: 'actions',
-        headerName: 'Actions',
-        width: 200,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: renderActionButtons
-    }
-];
-
-const rows = [
-    { id: 1, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.coms' },
-    { id: 2, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
-    { id: 3, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
-    { id: 4, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
-    { id: 5, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
-    { id: 6, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
-];
-
-
 export default function Employees() {
     const [value, setValue] = useState('1')
     const navigate = useNavigate()
@@ -101,7 +25,87 @@ export default function Employees() {
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    }
+
+    const handleToggleCheck = (e) => {
+        
+    }
+
+    const renderIsActiveToggle = (params) => {
+        return (
+            <div className="w-full h-full flex justify-center items-center">
+                <Toggle isCheck={false} returnCheck={(e) => handleToggleCheck(e)} />
+            </div>
+    
+        );
     };
+    
+    const renderActionButtons = (params) => {
+        return (
+            <div className="w-full h-full flex justify-center items-center">
+                <Button variant="text">
+                    <h1>Edit</h1>
+                </Button>
+            </div>
+        );
+    };
+    
+    const columns = [
+        {
+            field: 'id',
+            headerName: 'No.',
+            width: 90,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'userid',
+            headerName: 'ID',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'name',
+            headerName: 'Full Name',
+            width: 250,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            width: 250,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'isactive',
+            headerName: 'Active',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: renderIsActiveToggle
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: renderActionButtons
+        }
+    ];
+    
+    const rows = [
+        { id: 1, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.coms' },
+        { id: 2, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
+        { id: 3, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
+        { id: 4, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
+        { id: 5, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
+        { id: 6, userid: '000000012', name: 'Jon', email: 'yourpareng@gmail.com' },
+    ];
+    
     return (
         <>
             <div className="flex">
