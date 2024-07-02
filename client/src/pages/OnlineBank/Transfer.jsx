294c388475640ccd5793729e3bf43cd8093f9be3
sdevmarc <suanieeee@yahoo.com>
2024-07-02 21:44:55 +0800

    refactor: Add required attribute to debitaccount and creditaccount inputs in Transfer component

--- client/src/pages/OnlineBank/Transfer.jsx
+++ client/src/pages/OnlineBank/Transfer.jsx
@@ -41,6 +41,7 @@ export default function Transfer() {
                                             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">from/</span>
                                                 <input
+                                                    required
                                                     type="text"
                                                     name="debitaccount"
                                                     id="debitaccount"
@@ -59,6 +60,7 @@ export default function Transfer() {
                                             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">to/</span>
                                                 <input
+                                                    required
                                                     type="text"
                                                     name="creditaccount"
                                                     id="creditaccount"
