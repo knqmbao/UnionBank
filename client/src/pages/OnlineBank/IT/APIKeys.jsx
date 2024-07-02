72127506ce96f95e91250aefac3b411e4dcc7ed5
sdevmarc <suanieeee@yahoo.com>
2024-07-02 13:06:54 +0800

    refactor: Use sessionStorage instead of localStorage for credentials in APIKeys component

--- client/src/pages/OnlineBank/IT/APIKeys.jsx
+++ client/src/pages/OnlineBank/IT/APIKeys.jsx
@@ -12,7 +12,7 @@ export default function APIKeys() {
 
     const fetchCredentials = () => {
         try {
-            const credentials = localStorage.getItem('credentials')
+            const credentials = sessionStorage.getItem('credentials')
             if (!credentials) return navigate('/unionbank')
         } catch (error) {
             console.error(error)
