1d6325e9f3a7304c2918dc0add9c1852f20cd448
sdevmarc <suanieeee@yahoo.com>
2024-07-02 19:11:18 +0800

    feat: Add SyncAltOutlinedIcon to Sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -14,6 +14,7 @@ import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 import AccessibilityOutlinedIcon from '@mui/icons-material/AccessibilityOutlined';
 import AddCardOutlinedIcon from '@mui/icons-material/AddCardOutlined';
 import HourglassEmptyOutlinedIcon from '@mui/icons-material/HourglassEmptyOutlined';
+import SyncAltOutlinedIcon from '@mui/icons-material/SyncAltOutlined';
 
 export default function Sidebar() {
     const [userRole, setUserRole] = useState('')
@@ -106,7 +107,7 @@ export default function Sidebar() {
                                             </h1>
                                         </NavLink>
                                         <NavLink to='/ledger' className="w-full flex justify-start items-center gap-[1rem] px-[1rem] py-[.7rem] rounded-md scale-[.7] sm:scale-[.8] md:scale-[.9] lg:scale-[1]">
-                                            <AccessibilityOutlinedIcon />
+                                            <SyncAltOutlinedIcon />
                                             <h1 className='hidden sm:hidden md:hidden lg:block text-[#3D4751] text-[.7rem] sm:text-[.8rem] md:text-[.7rem] lg:text-[.9rem]'>
                                                 Ledger
                                             </h1>
