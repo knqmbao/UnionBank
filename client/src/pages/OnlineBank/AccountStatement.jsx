337ab1b916949a706bdb6dbd6965aa7153487e4d
sdevmarc <suanieeee@yahoo.com>
2024-07-02 13:06:15 +0800

    refactor: Use sessionStorage instead of localStorage for credentials in AccountStatement component

--- client/src/pages/OnlineBank/AccountStatement.jsx
+++ client/src/pages/OnlineBank/AccountStatement.jsx
@@ -12,7 +12,7 @@ export default function AccountStatement() {
 
     const fetchCredentials = () => {
         try {
-            const credentials = localStorage.getItem('credentials')
+            const credentials = sessionStorage.getItem('credentials')
             if (!credentials) return navigate('/unionbank')
         } catch (error) {
             console.error(error)
