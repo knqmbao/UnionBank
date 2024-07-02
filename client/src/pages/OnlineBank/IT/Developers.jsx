ba596b58a728f8d0d1a3dcc654d609c2cb4fce90
sdevmarc <suanieeee@yahoo.com>
2024-07-02 13:07:02 +0800

    refactor: Use sessionStorage instead of localStorage for credentials in Developers component

--- client/src/pages/OnlineBank/IT/Developers.jsx
+++ client/src/pages/OnlineBank/IT/Developers.jsx
@@ -74,7 +74,7 @@ export default function Developers() {
 
     const fetchCredentials = () => {
         try {
-            const credentials = localStorage.getItem('credentials')
+            const credentials = sessionStorage.getItem('credentials')
             if (!credentials) return navigate('/unionbank')
         } catch (error) {
             console.error(error)
