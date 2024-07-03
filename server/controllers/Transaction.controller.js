c9121cb91b8abfd5ff53479e13c45bfe57eed12a
sdevmarc <suanieeee@yahoo.com>
2024-07-03 19:14:52 +0800

    refactor: Format transaction createdAt date with time in TransactionController

--- server/controllers/Transaction.controller.js
+++ server/controllers/Transaction.controller.js
@@ -104,7 +104,16 @@ const TransactionController = {
 
             const formattedData = data.map(transaction => {
                 const { createdAt, _id, amount, description, transactionType, balance } = transaction;
-                const formattedCreatedAt = new Date(createdAt).toLocaleDateString('en-US');
+
+                // Format date including time
+                const formattedCreatedAt = new Date(createdAt).toLocaleString('en-US', {
+                    month: '2-digit',
+                    day: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit'
+                });
+
                 return { _id, amount, description, createdAt: formattedCreatedAt, transactionType, balance };
             });
 
