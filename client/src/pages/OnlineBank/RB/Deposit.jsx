207915c25c44f9f1f8ea084b98a8700c0e98e2ab
sdevmarc <suanieeee@yahoo.com>
2024-07-03 19:32:44 +0800

    refactor: Remove placeholder values for account and amount fields in Deposit component

--- client/src/pages/OnlineBank/RB/Deposit.jsx
+++ client/src/pages/OnlineBank/RB/Deposit.jsx
@@ -91,7 +91,6 @@ export default function Deposit() {
                                                     id="account"
                                                     autoComplete="account"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                    placeholder="4757****"
                                                     required
                                                 />
                                             </div>
@@ -112,7 +111,6 @@ export default function Deposit() {
                                                     id="amount"
                                                     autoComplete="amount"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                    placeholder="100.00"
                                                     required
                                                 />
                                             </div>
