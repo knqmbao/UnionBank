065d1cadbc77249cdf14992a01e5d23e742abd1b
sdevmarc <suanieeee@yahoo.com>
2024-07-03 17:23:15 +0800

    refactor: Add Statement component for displaying user transactions

---
+++ client/src/pages/OnlineBank/Statement.jsx
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../../components/Sidebar';
+import Header__Dashboard from '../../components/Header__dashboard';
+import DataGrids from '../../components/DataGrids';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+const { VITE_HOST, VITE_ADMIN_TOKEN } = import.meta.env
+
+export default function Transactions() {
+    const [value, setValue] = useState('1');
+    const [userTransactions, setUserTransactions] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetchCredentials()
+        fetchUserTransactions()
+    }, [])
+
+    const fetchCredentials = () => {
+        try {
+            const credentials = sessionStorage.getItem('credentials')
+            if (!credentials) return navigate('/unionbank')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const fetchUserTransactions = async () => {
+        try {
+            const credentials = sessionStorage.getItem('credentials')
+            const { userId } = JSON.parse(credentials)
+
+            const res = await axios.get(`${VITE_HOST}/api/transactions/${userId}`, {
+                headers: {
+                    Authorization: `Bearer ${VITE_ADMIN_TOKEN}`
+                }
+            })
+
+            const transactions = res?.data?.data
+            const formattedData = transactions.map((trans, index) => ({
+                id: index + 1,
+                date: trans?.createdAt,
+                reference: trans?._id,
+                debit: trans?.amount,
+                credit: trans?.amount,
+                description: trans?.description,
+                transactionType: trans?.transactionType,
+                balance: trans?.balance
+            }))
+            setUserTransactions(formattedData)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const renderDebitCell = (params) => {
+        return (
+            <div className="w-full h-full flex justify-center items-center">
+                {
+                    (params.row.transactionType === 'withdrawal' || params.row.transactionType === 'transfer_debit') ? params.row.debit : '---'
+                }
+            </div>
+
+        );
+    };
+
+    const renderCreditCell = (params) => {
+        return (
+            <div className="w-full h-full flex justify-center items-center">
+                <h1 className='font-bold'>
+                    {
+                        (params.row.transactionType === 'deposit' || params.row.transactionType === 'transfer_credit') ? params.row.credit : '---'
+                    }
+                </h1>
+            </div>
+
+        );
+    };
+
+    const UserColumns = [
+        {
+            field: 'id',
+            headerName: 'No.',
+            width: 90,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'date',
+            headerName: 'Date',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'reference',
+            headerName: 'Reference',
+            type: 'number',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'debit',
+            headerName: 'Debit (PHP)',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: renderDebitCell
+        },
+        {
+            field: 'credit',
+            headerName: 'Credit (PHP)',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: renderCreditCell
+        },
+        {
+            field: 'balance',
+            headerName: 'Balance',
+            width: 300,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'description',
+            headerName: 'Description',
+            width: 300,
+            headerAlign: 'center',
+            align: 'center'
+        }
+    ]
+
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="w-[80%] h-screen flex flex-col justify-start items-start p-[1rem] overflow-auto">
+                    <Header__Dashboard title={`View Statement`} />
+                    <div className="w-full h-[95%] flex flex-col justify-start items-start gap-[1rem]">
+                        <div className="w-full h-[5%]">
+                            <h1 className='text-black font-[600] text-[1.2rem]'>
+                                Transaction History
+                            </h1>
+                        </div>
+                        <div className="w-full h-[90%]">
+                            <DataGrids columnsTest={UserColumns} rowsTest={userTransactions} descCol={`id`} colVisibility={{ id: false }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
