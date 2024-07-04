d8fa1d7e892625b33030d0f9388301b041ee4f37
sdevmarc <suanieeee@yahoo.com>
2024-07-04 20:31:13 +0800

    refactor: Update HeaderDashboard component to use dynamic breadcrumbs

--- client/src/components/Header__dashboard.jsx
+++ client/src/components/Header__dashboard.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
+import React from 'react';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Link from '@mui/material/Link';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 
-export default function Header__Dashboard({ linkName,linkName1, link, link1, title }) {
-    const breadcrumbs = [
-        linkName &&
-        <Link underline="hover" key="1" fontSize={`.9rem`} href={link}>
-            {linkName}
-        </Link>,
-         linkName1 &&
-         <Link underline="hover" key="1" fontSize={`.9rem`} href={link1}>
-             {linkName1}
-         </Link>,
-        <h1 key="3" className='font-[500] text-[.9rem] text-[#111111]'>
-            {title}
-        </h1>,
-    ];
+export default function HeaderDashboard({ breadcrumbs }) {
     return (
-        <>
-            <div className="w-full h-[5%] flex justify-start items-center">
-                <Breadcrumbs
-                    separator={<NavigateNextIcon fontSize="small" />}
-                >
-                    {breadcrumbs}
-                </Breadcrumbs>
-            </div>
-        </>
-    )
-}
+        <div className="w-full h-[5%] flex justify-start items-center">
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />}>
+                {
+                    breadcrumbs.map((item, i) =>
+                        item?.isLink ? (
+                            <Link underline="hover" key={i} fontSize={`.9rem`} href={item?.href}>
+                                {item?.title}
+                            </Link>
+                    ) : (
+                            <h1 key={i} className='font-[500] text-[.9rem] text-[#111111]'>
+                                {item?.title}
+                            </h1>
+                    )
+                )}
+            </Breadcrumbs>
+        </div>
+    );
+} 
