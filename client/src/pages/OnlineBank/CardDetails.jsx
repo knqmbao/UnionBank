d14f4a58474d088c2341e7e5c7390d3e57df1085
Marc Edison <suanieeee@yahoo.com>
2024-07-06 03:55:32 +0800

    refactor: Update CardDetails component to display current balance with proper formatting

--- client/src/pages/OnlineBank/CardDetails.jsx
+++ client/src/pages/OnlineBank/CardDetails.jsx
@@ -77,8 +77,7 @@ export default function AccountStatement() {
                                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                     <dt className="text-sm font-medium leading-6 text-gray-900">Current Balance</dt>
                                     <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                        PHP
-                                        {carddetails?.balance}
+                                        PHP {carddetails?.balance}
                                     </dd>
                                 </div>
                                 <div className="w-full flex items-center justify-end gap-x-6 pt-[2rem]">
