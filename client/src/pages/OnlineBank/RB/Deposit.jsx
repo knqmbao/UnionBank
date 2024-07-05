bb4663e79fabc588193ff1a9a041c0779b3c2f2d
sdevmarc <suanieeee@yahoo.com>
2024-07-05 19:06:53 +0800

    refactor: Add account ID parameter to Deposit page and fetch account number

--- client/src/pages/OnlineBank/RB/Deposit.jsx
+++ client/src/pages/OnlineBank/RB/Deposit.jsx
@@ -1,25 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import Sidebar from '../../../components/Sidebar'
 import Header__Dashboard from '../../../components/Header__dashboard'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 const { VITE_HOST, VITE_ADMIN_TOKEN } = import.meta.env
 
 export default function Deposit() {
+    const { accountid } = useParams()
     const [values, setValues] = useState({
         account: '',
         amount: '',
     })
+
     const navigate = useNavigate()
 
     useEffect(() => {
         fetchCredentials()
+        fetchAccountNo()
     }, [])
 
     const fetchCredentials = () => {
         try {
             const credentials = sessionStorage.getItem('credentials')
             if (!credentials) return navigate('/unionbank')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const fetchAccountNo = async () => {
+        try {
+            const res = await axios.get(`${VITE_HOST}/api/useraccount/${accountid}`, {
+                headers: {
+                    Authorization: `Bearer ${VITE_ADMIN_TOKEN}`
+                }
+            })
+            const accountno = res?.data?.data?.accountno
+            setValues((prev) => ({
+                ...prev,
+                account: accountno
+            }))
         } catch (error) {
             console.error(error)
         }
@@ -57,9 +77,12 @@ export default function Deposit() {
     const handleCleanUp = () => {
         setValues((prev) => ({
             ...prev,
-            account: '',
             amount: ''
         }))
+    }
+
+    const handleBack = () => {
+        navigate('/ledger')
     }
 
     return (
@@ -77,7 +100,6 @@ export default function Deposit() {
                                 <p className="mt-1 text-sm leading-6 text-gray-600">
                                     Please provide information for deposit.
                                 </p>
-
                                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                     <div className="sm:col-span-3">
                                         <label htmlFor="creditaccount" className="block text-sm font-medium leading-6 text-gray-900">
@@ -88,13 +110,12 @@ export default function Deposit() {
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">to/</span>
                                                 <input
                                                     value={values?.account}
-                                                    onChange={handleOnChange}
                                                     type="text"
                                                     name="account"
                                                     id="account"
                                                     autoComplete="account"
                                                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                    required
+                                                    readOnly
                                                 />
                                             </div>
                                         </div>
@@ -122,6 +143,11 @@ export default function Deposit() {
                                 </div>
                             </div>
                             <div className="w-full flex items-center justify-end gap-x-6">
+                            <button
+                                    onClick={handleBack}
+                                    className="text-sm font-semibold leading-6 text-gray-900">
+                                    Back
+                                </button>
                                 <button
                                     type="submit"
                                     className="rounded-md bg-[#111111] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#333333] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -131,9 +157,7 @@ export default function Deposit() {
                             </div>
                         </div>
                     </form>
-                    <div className="w-full h-[100vh] bg-black">
 
-                    </div>
                 </div>
             </div >
         </>
@@ -141,6 +165,6 @@ export default function Deposit() {
 }
 
 const breadCrumbs = [
-    // { title: 'Home', href: '/', isLink: true },
+    { title: 'View Accounts', href: '/ledger', isLink: true },
     { title: 'Deposit', isLink: false },
 ]
