fc67917393d3bf16bdcf5bef326b3c89a7f97d8b
sdevmarc <suanieeee@yahoo.com>
2024-07-09 20:56:35 +0800

    refactor: Remove unused state and update transaction search functionality in Ledger component

--- client/src/pages/OnlineBank/RB/Ledger.jsx
+++ client/src/pages/OnlineBank/RB/Ledger.jsx
@@ -8,11 +8,6 @@ const { VITE_HOST, VITE_ADMIN_TOKEN } = import.meta.env
 
 export default function Ledger() {
     const { accountid } = useParams()
-    const [values, setValues] = useState([])
-    const [details, setDetails] = useState({
-        role: '',
-        uid: ''
-    })
     const [userTransactions, setUserTransactions] = useState([])
     const navigate = useNavigate()
 
@@ -28,13 +23,6 @@ export default function Ledger() {
         try {
             const credentials = sessionStorage.getItem('credentials')
             if (!credentials) return navigate('/unionbank')
-            const { userId, role } = JSON.parse(credentials)
-            setDetails((prev) => ({
-                ...prev,
-                role: role,
-                uid: userId
-            }))
-
         } catch (error) {
             console.error(error)
         }
@@ -63,6 +51,38 @@ export default function Ledger() {
             setUserTransactions(formattedData)
         } catch (error) {
             console.error(error)
+        }
+    }
+
+    const handleSearchTransaction = async (e) => {
+        try {
+            const { value } = e.target
+            if (value === '') return fetchUserTransactions()
+
+            const res = await axios.get(`${VITE_HOST}/api/searchtransactions/${value}`, {
+                headers: {
+                    Authorization: `Bearer ${VITE_ADMIN_TOKEN}`
+                }
+            })
+            if (res?.data?.success) {
+                const transactions = res?.data?.data
+                const formattedData = transactions.map((trans, index) => ({
+                    id: index + 1,
+                    date: trans?.createdAt,
+                    reference: trans?._id,
+                    debit: trans?.amount,
+                    credit: trans?.amount,
+                    description: trans?.description,
+                    transactionType: trans?.transactionType,
+                    servicefee: trans?.fee,
+                    balance: trans?.balance
+                }))
+                setUserTransactions(formattedData)
+            } else {
+                setUserTransactions([])
+            }
+        } catch (error) {
+            console.error()
         }
     }
 
@@ -167,6 +187,7 @@ export default function Ledger() {
                                 Search
                             </h1>
                             <input
+                                onChange={handleSearchTransaction}
                                 type="text"
                                 className="block w-[20rem] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder='Search here...'
