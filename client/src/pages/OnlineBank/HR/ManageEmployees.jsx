4249f611660a2d8b6ac796a341dd8dad52d2842e
sdevmarc <suanieeee@yahoo.com>
2024-07-10 23:01:17 +0800

    refactor: Improve input styling in ManageEmployees component

--- client/src/pages/OnlineBank/HR/ManageEmployees.jsx
+++ client/src/pages/OnlineBank/HR/ManageEmployees.jsx
@@ -169,7 +169,7 @@ export default function ManageEmployees() {
                                                 name="name"
                                                 id="name"
                                                 autoComplete="name"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
@@ -187,7 +187,7 @@ export default function ManageEmployees() {
                                                 name="email"
                                                 type="email"
                                                 autoComplete="email"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
@@ -204,7 +204,7 @@ export default function ManageEmployees() {
                                                 name="mobileno"
                                                 type="text"
                                                 inputMode='numeric'
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                         <div className="mt-2">
