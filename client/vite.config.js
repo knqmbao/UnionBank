08421601db05c27893a00524b53e3a90a4c9cbba
sdevmarc <suanieeee@yahoo.com>
2024-07-10 22:59:16 +0800

    refactor: Update vite.config.js to include alias for "@/src" directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    // host: '192.168.10.14',
-    // port: '80'
-    host: 'localhost',
-  }
+    plugins: [react()],
+    server: {
+        // host: '192.168.10.14',
+        // port: '80'
+        // host: '192.168.1.7',
+        // port: '80'
+        host: 'localhost',
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
