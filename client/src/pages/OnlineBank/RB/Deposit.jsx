8fdad844af6a46950625170c64e167e15179461a
sdevmarc <suanieeee@yahoo.com>
2024-07-10 23:01:58 +0800

    refactor: Improve input styling in Deposit component

--- client/src/pages/OnlineBank/RB/Deposit.jsx
+++ client/src/pages/OnlineBank/RB/Deposit.jsx
@@ -109,7 +109,7 @@ export default function Deposit() {
                                             Account No.
                                         </label>
                                         <div className="mt-2">
-                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">to/</span>
                                                 <input
                                                     value={values?.account}
@@ -117,7 +117,7 @@ export default function Deposit() {
                                                     name="account"
                                                     id="account"
                                                     autoComplete="account"
-                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                                     readOnly
                                                 />
                                             </div>
@@ -128,7 +128,7 @@ export default function Deposit() {
                                             Amount
                                         </label>
                                         <div className="mt-2">
-                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
                                                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">PHP/</span>
                                                 <input
                                                     value={values?.amount}
@@ -137,7 +137,7 @@ export default function Deposit() {
                                                     name="amount"
                                                     id="amount"
                                                     autoComplete="amount"
-                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                                     required
                                                 />
                                             </div>
