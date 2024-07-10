32fa06a63287fbf5ceb3627a516c6ae64163dcee
sdevmarc <suanieeee@yahoo.com>
2024-07-10 19:55:09 +0800

    refactor: Update Login component to use correct LogoUB image

--- client/src/pages/Home/Login.jsx
+++ client/src/pages/Home/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import LogoUB from '../../assets/LogoUB1.png'
+import LogoUB from '../../assets/LogoUB.png'
 import LoginBG from '../../assets/LoginBG.png'
 import axios from 'axios'
 const { VITE_HOST, VITE_ADMIN_TOKEN } = import.meta.env
@@ -63,12 +63,13 @@ export default function Login() {
     }
     return (
         <>
-            <div className="w-full h-screen flex flex-col justify-start items-center">
+            <div className="bg-[#121212] w-full h-screen flex flex-col justify-start items-center">
                 <div className="w-full h-full flex justify-start items-center">
                     <div className="w-[70%] h-full flex justify-start items-center">
                         <img src={LoginBG} alt="BG" className='w-full h-full object-cover' />
                     </div>
                     <div className="w-[30%] h-full flex justify-center items-center">
+
                         <form
                             onSubmit={handleLogin}
                             className='w-full h-full flex flex-col justify-center items-center gap-[1rem] px-[4rem]'>
@@ -76,23 +77,23 @@ export default function Login() {
                                 <img src={LogoUB} alt="BG" className='w-full h-full object-contain' />
                             </div>
                             <div className="w-full flex flex-col">
-                                <h1>Email</h1>
-                                <input onChange={handleOnChange} type="email" required name='email' className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your email...'/>
+                                <h1 className='text-white'>Email</h1>
+                                <input onChange={handleOnChange} type="email" required name='email' className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your email...' />
                             </div>
                             <div className="w-full flex flex-col">
-                                <h1>Password</h1>
+                                <h1 className='text-white'>Password</h1>
                                 <input onChange={handleOnChange} type="password" required name='password' className='px-[1rem] rounded-md placeholder:text-[.8rem]' placeholder='Enter your password...' />
                             </div>
-                            <button type='submit' className='w-full py-[.6rem] rounded-lg text-[#7b7b7b] hover:bg-[#111111] hover:text-white duration-300 ease bg-[#dcdcdc] shadow-[_0_10px_15px_-3px_rgba(0,0,0,0.1)]'>
+                            <button type='submit' className={`w-full py-[.6rem] rounded-lg  ${values?.email && values?.password ? 'text-[#ffffff]' : 'text-[#7b7b7b]'} hover:bg-[#007eff] hover:text-white duration-300 ease ${values?.email && values?.password ? 'bg-[#007eff]' : 'bg-[#dcdcdc]'} shadow-[_0_10px_15px_-3px_rgba(0,0,0,0.1)]`}>
                                 Login
                             </button>
                             <div className="w-full flex flex-col justify-start items-start">
-                                <p className='text-[.8rem]'>
-                                    Don't have an account? <span className='cursor-pointer text-black text-decoration-line: underline' onClick={handleSignUp}>Sign Up.</span>
+                                <p className='text-[.8rem] text-white'>
+                                    Don't have an account? <span className='cursor-pointer text-white text-decoration-line: underline' onClick={handleSignUp}>Sign Up.</span>
                                 </p>
-                                <p className='cursor-pointer text-[.8rem] text-black text-decoration-line: underline'>
+                                {/* <p className='cursor-pointer text-[.8rem] text-decoration-line: underline text-white'>
                                     Forgot password?
-                                </p>
+                                </p> */}
                             </div>
                         </form>
                     </div>
