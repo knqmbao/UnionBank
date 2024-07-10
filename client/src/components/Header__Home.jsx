6b621d809fb6e97dacac732ee01f851ff5eb5c99
sdevmarc <suanieeee@yahoo.com>
2024-07-10 19:54:52 +0800

    refactor: Update Header__Home component to use NavLink instead of Link for navigation

--- client/src/components/Header__Home.jsx
+++ client/src/components/Header__Home.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import Logo from '../assets/LogoUB.png'
-import { NavLink } from 'react-router-dom'
-import './css/Header_home.css'
+import { Link } from 'react-router-dom'
 
 export default function Header__Home() {
     return (
         <>
-            <div className="fixed w-full h-[8%] flex justify-between items-center px-[25rem] z-[1]">
-                <div className="w-[12rem] h-full">
-                    <img src={Logo} alt="Logo" className='w-full h-full object-contain' />
+            <div className="fixed w-full h-[8%] flex justify-between items-center px-[7rem] sm:px-[10rem] md:px-[15rem] lg:px-[20rem] z-[1]">
+                <div className="w-[50%] h-full flex justify-start items-center gap-[1rem]">
+                    <div className="w-[12rem] h-full">
+                        <img src={Logo} alt="Logo" className='w-full h-full object-contain' />
+                    </div>
+                    <Link to={`/unionbank`} className='text-[#7ba1bf] font-[500] text-[.9rem] hover:text-white duration-300 ease'>
+                        Terms of Use
+                    </Link>
+                    <Link to={`/unionbank`} className='text-[#7ba1bf] font-[500] text-[.9rem] hover:text-white duration-300 ease'>
+                        Privacy Policy
+                    </Link>
                 </div>
-                <div className="hhome h-full flex justify-center items-center gap-[3rem]">
-                    <NavLink to={`/unionbank`} className='text-[#74878e] font-[500] text-[.9rem]'>
-                        Home
-                    </NavLink>
-                    <NavLink to={`/login`} className='text-white font-[500] text-[.9rem]'>
-                        About Us
-                    </NavLink>
-                    <NavLink to={`/login`} className='text-white font-[500] text-[.9rem]'>
-                        Contact Us
-                    </NavLink>
-                    <NavLink to={`/login`} className='text-white font-[500] text-[.9rem]'>
+
+                <div className="w-[50%] h-full flex justify-end items-center gap-[.7rem] sm:gap-[1rem] md:gap-[1.5rem] lg:gap-[.8rem]">
+                    <Link to={`/signup`} className='text-white px-[1rem] py-[.6rem] bg-[#1daeef] rounded-xl font-[600] text-[.8rem] hover:bg-[#58caff]'>
+                        Sign Up
+                    </Link>
+                    <Link to={`/login`} className='text-white px-[1rem] py-[.6rem] border-[1px] border-white rounded-xl font-[500] text-[.8rem]  hover:bg-[#ffffff] hover:text-black'>
                         Login
-                    </NavLink>
+                    </Link>
                 </div>
             </div>
         </>
