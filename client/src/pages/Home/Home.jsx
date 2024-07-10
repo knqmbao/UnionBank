e76c02be0050ce25faed524f7829082457216e81
sdevmarc <suanieeee@yahoo.com>
2024-07-10 19:55:03 +0800

    refactor: Optimize image positioning and responsiveness in Home component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,18 +8,18 @@ export default function Home() {
         <div className='w-full h-screen flex justify-start items-center flex-col bg-[#121212]'>
             <Header__Home />
             <div className="overflow-hidden w-full h-full flex flex-col justify-evenly items-center relative">
-                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[-2rem] left-[-10rem] w-[20rem] h-[10rem] scale-[5]' />
-                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[10rem] right-[-9rem] w-[20rem] h-[10rem] scale-[5]' />
+                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[-2rem] left-[30rem] sm:left-[25rem] md:left-[20rem] lg:-left-[15rem] w-[20rem] h-[10rem] scale-[5]' />
+                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[10rem] right-[-30rem] sm:right-[-25rem] md:right-[-20rem] lg:right-[-15rem] w-[20rem] h-[10rem] scale-[5]' />
                 <div className="w-full flex flex-col justify-center items-center gap-[1rem]">
-                    <h1 className='text-white text-[4rem] font-[600] text-center'>
+                    <h1 className='text-white text-[4rem] font-[600] text-center z-[1]'>
                         Powerful for developers. <br /> Fast for everyone.
                     </h1>
-                    <div className="w-[30%]">
+                    <div className="w-full">
                         <h1 className='text-[#87878e] text-center'>
                             Bring blockchain to the people. UnionBank supports experiences <br /> for power users, new consumers, and everyone in between.
                         </h1>
                     </div>
-                    <div className="w-full flex justify-center items-center gap-[1rem]">
+                    <div className="w-full flex justify-center items-center gap-[1rem] z-[1]">
                         <Link to={`https://spiritual-wire-287.notion.site/UnionBank-API-Documentation-6a2928ba55e5442b91423fda3ebd8f78?pvs=4`}
                             className='text-white px-[1.7rem] py-[.7rem] border-white border-[1px] rounded-3xl hover:bg-white hover:text-black'
                             target='_blank'
