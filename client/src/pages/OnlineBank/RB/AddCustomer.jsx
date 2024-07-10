72e52d25a76bc51e9ee2f7847070087eb532a63b
sdevmarc <suanieeee@yahoo.com>
2024-07-10 23:01:40 +0800

    refactor: Improve input styling in AddCustomer component

--- client/src/pages/OnlineBank/RB/AddCustomer.jsx
+++ client/src/pages/OnlineBank/RB/AddCustomer.jsx
@@ -67,7 +67,7 @@ export default function AddCustomer() {
                                                 name="firstname"
                                                 id="firstname"
                                                 autoComplete="given-name"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
@@ -83,7 +83,7 @@ export default function AddCustomer() {
                                                 name="lastname"
                                                 id="lastname"
                                                 autoComplete="family-name"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
@@ -100,7 +100,7 @@ export default function AddCustomer() {
                                                 name="email"
                                                 type="email"
                                                 autoComplete="email"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
@@ -116,7 +116,7 @@ export default function AddCustomer() {
                                                 name="mobileno"
                                                 type="text"
                                                 inputMode='numeric'
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                             />
                                         </div>
                                     </div>
