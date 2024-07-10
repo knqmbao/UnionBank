c2111825fed287790de59f2d3e1f3625a4cc57cc
sdevmarc <suanieeee@yahoo.com>
2024-07-10 23:01:45 +0800

    refactor: Improve input styling in Customers component

--- client/src/pages/OnlineBank/RB/Customers.jsx
+++ client/src/pages/OnlineBank/RB/Customers.jsx
@@ -371,7 +371,7 @@ export default function Customers() {
                                     <input
                                         onChange={handleOnChangeRBUsers}
                                         type="text"
-                                        className="block w-[20rem] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        className="block w-[20rem] rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                         placeholder='Search here...'
                                     />
                                 </div>
@@ -380,7 +380,7 @@ export default function Customers() {
                                         to={`/customers/addcustomer`}
                                         className="rounded-md bg-[#111111] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#333333] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     >
-                                        Open Account
+                                        Open Customer
                                     </Link>
                                 </div>
                             </div>
@@ -397,7 +397,7 @@ export default function Customers() {
                                     <input
                                         onChange={handleOnChangeRBAccounts}
                                         type="text"
-                                        className="block w-[20rem] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        className="block w-[20rem] rounded-md border-0 px-[.7rem] py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                                         placeholder='Search here...'
                                     />
                                 </div>
