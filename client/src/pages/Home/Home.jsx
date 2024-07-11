1277f74b9457625cfb7129490b3c5fbf9e6d82e2
sdevmarc <suanieeee@yahoo.com>
2024-07-11 13:26:59 +0800

    feat: Add maintenance break overlay on home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,6 +10,9 @@ export default function Home() {
             <div className="overflow-hidden w-full h-full flex flex-col justify-evenly items-center relative">
                 <img src={SolanaPic} alt="Solana Pic" className='absolute top-[-2rem] left-[30rem] sm:left-[25rem] md:left-[20rem] lg:-left-[15rem] w-[20rem] h-[10rem] scale-[5]' />
                 <img src={SolanaPic} alt="Solana Pic" className='absolute top-[10rem] right-[-30rem] sm:right-[-25rem] md:right-[-20rem] lg:right-[-15rem] w-[20rem] h-[10rem] scale-[5]' />
+                <div className="absolute w-full h-screen bg-[rgba(0,0,0,0.54)] flex justify-center items-center z-[2]">
+                    <h1 className='text-white text-[4rem] text-bold'>Maintenance break, we will be right back</h1>
+                </div>
                 <div className="w-full flex flex-col justify-center items-center gap-[1rem]">
                     <h1 className='text-white text-[4rem] font-[600] text-center z-[1]'>
                         Powerful for developers. <br /> Fast for everyone.
