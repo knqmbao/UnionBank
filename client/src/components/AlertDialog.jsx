f6c067734ad8379d844ef62e765059449f6ad926
sdevmarc <suanieeee@yahoo.com>
2024-07-13 17:05:17 +0800

    refactor: Add AlertDialog component for displaying confirmation dialogs

---
+++ client/src/components/AlertDialog.jsx
@@ -0,0 +1,30 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export function AlertDialogs({ open, onClose, onConfirm }) {
+    return (
+        <AlertDialog open={open} onOpenChange={onClose}>       
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently transfer funds to another account.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                <AlertDialogCancel onClick={onClose}>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
