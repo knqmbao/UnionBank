566e6432e28eabdf545b2bc6e378c4f751d33738
sdevmarc <suanieeee@yahoo.com>
2024-07-13 23:50:22 +0800

    refactor: Add socket.io server for real-time messaging

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,26 @@
-const app = require('./App');
+const app = require('./App')
+const { Server } = require('socket.io');
+const server = require('http').createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    // console.log('A user connected:', socket.id);
+
+    socket.on('send_message', (data) => {
+        socket.broadcast.emit('receive_message', data)
+    });
+});
 
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.prodHOST
+const HOST = process.env.devHOST;
 
-app.listen(PORT, HOST, () => {
+server.listen(PORT, HOST, () => {
     console.log(`Listening: http://${HOST}:${PORT}`);
 });
+
