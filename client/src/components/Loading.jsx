9d4366bcfb3070803af927cb64e6eda7da3e1642
Marc Edison <suanieeee@yahoo.com>
2024-07-14 03:19:36 +0800

    refactor: Update Loading component to add z-index for overlay

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Loading() {
     return (
-        <div className="absolute w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.6)]">
+        <div className="absolute w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.6)] z-[100]">
             <CircularProgress />
         </div>
     )
