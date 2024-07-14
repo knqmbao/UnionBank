abedc3784e7e544bd396c95ed6d9dcf19015abe3
sdevmarc <suanieeee@yahoo.com>
2024-07-14 21:10:11 +0800

    refactor: Update server to use app.listen instead of server.listen

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,9 @@
 const app = require('./App')
-const { Server } = require('socket.io');
-const server = require('http').createServer(app)
-
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-})
-
-io.on('connection', (socket) => {
-    // console.log('A user connected:', socket.id);
-
-    socket.on('send_message', (data) => {
-        socket.broadcast.emit('receive_message', data)
-    });
-});
 
 const PORT = process.env.PORT || 3001;
 const HOST = process.env.devHOST;
 
-server.listen(PORT, HOST, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Listening: http://${HOST}:${PORT}`);
 });
 
